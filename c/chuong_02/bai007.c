79d435249f7e73cb9592de5081dd31e8b7647f0c
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 04:31:00 +0700

    Create bai007.c

---
+++ c/chuong_02/bai007.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef long long ll;
+
+int main() {
+	int a;
+	scanf("%d", &a);
+	int b = 0;
+	for (int i = 1; i * i <= a; i++) {
+		if (a%i == 0) {
+			b += 2;
+		}
+	}
+	int c = sqrt(a);
+	if (c * c == a) b -= 1;
+	printf("%d \n", b);
+	for (int i = 1; i * i < a; i++) {
+		if (a%i == 0) {
+			printf("%d ", i);
+		}
+	}
+	for (int i = c; i > 0; i--) {
+		if (a%i == 0) {
+			printf("%d ", a / i);
+		}
+	}
+	printf("\n");
+	return 0;
+}
