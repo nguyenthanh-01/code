1e570b4568bf17b0649c5f45801fe169b4094ff4
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 13:47:33 +0700

    Update bai007.c

--- c/chuong_01/bai007.c
+++ c/chuong_01/bai007.c
@@ -5,8 +5,8 @@ typedef long long ll;
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
-    int ac1 = a / b * b;
-    int ac2 = (a + b - 1) / b * b;
-    printf("%d \n%d \n", ac1, ac2);
+    int ans1 = a / b * b;
+    int ans2 = (a + b - 1) / b * b;
+    printf("%d \n%d \n", ans1, ans2);
     return 0;
 }
