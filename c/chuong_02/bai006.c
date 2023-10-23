ea1b0e06f436c42b1024210859f9e84406c3d27b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 14:23:48 +0700

    Update bai006.c

--- c/chuong_02/bai006.c
+++ c/chuong_02/bai006.c
@@ -6,15 +6,15 @@ typedef long long ll;
 int main() {
 	ll a;
 	scanf("%lld", &a);
-	ll ac1 = 0;
+	ll res1 = 0;
 	for (ll i = 1; i * i <= a; i++) {
 		if (a%i == 0) {
-			ac1 += i;
-			ac1 += (a / i);
+			res1 += i;
+			res1 += (a / i);
 		}
 	}
-	ll ac2 = sqrt(a);
-	if (ac2 * ac2 == a) ac1 -= ac2;
-	printf("%lld \n", ac1);
+	ll res2 = sqrt(a);
+	if (res2 * res2 == a) res1 -= res2;
+	printf("%lld \n", res1);
 	return 0;
 }
