23e3748ec37c4e95accda9b0e5f419a2ad5ef284
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 00:11:46 +0700

    Update bai009.c

--- c/chuong_02/bai009.c
+++ c/chuong_02/bai009.c
@@ -9,13 +9,12 @@ int main() {
     ll res = 1;
     for (ll i = 1; i * i <= a; i++) {
     	if (a%i == 0) {
-    		res *= i;
-    		res *= (a / i);
+    	    res *= i;
+    	    res *= (a / i);
 		}
-	}
-	ll tmp = sqrt(a);
-	if (tmp * tmp == a) res /= tmp;
-	printf("%lld \n", res);
+    }
+    ll tmp = sqrt(a);
+    if (tmp * tmp == a) res /= tmp;
+    printf("%lld \n", res);
     return 0;
 }
- 
