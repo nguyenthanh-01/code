8ec5d388f2df302538dbf15925000cc75383a9f7
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 23:31:09 +0700

    Create bai019.c

---
+++ c/chuong_02/bai019.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef long long ll;
+
+int main() {
+	ll a;
+	scanf("%lld", &a);
+	a /= 28;
+	int res = a;
+	while (3 <= a) {
+		res += a / 3;
+		int tmp = a % 3;
+		a = a / 3 + tmp;
+	}
+	printf("%d", res);
+	return 0;
+}
