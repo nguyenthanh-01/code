475531491ce599d83242ecbbbe59db3dbd0faeae
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 16:35:03 +0700

    Create bai015.c

---
+++ c/chuong_02/bai015.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef long long ll;
+
+int main() {
+	int a;
+	scanf("%d", &a);
+	ll res = 1;
+	for (int i = 1; i <= a; i++) {
+		res *= i;
+	}
+	printf("%lld \n", res);
+	return 0;
+}
