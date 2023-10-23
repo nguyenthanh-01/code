60a6dd6b75f6a0a9e098ba893c7f0e5a5b99b811
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 10:06:02 +0700

    Update bai007.c

--- c/chuong_02/bai007.c
+++ c/chuong_02/bai007.c
@@ -6,25 +6,25 @@ typedef long long ll;
 int main() {
 	int a;
 	scanf("%d", &a);
-	int b = 0;
+	int ac1 = 0;
 	for (int i = 1; i * i <= a; i++) {
 		if (a%i == 0) {
-			b += 2;
+			ac1 += 2;
 		}
 	}
-	int c = sqrt(a);
-	int ac = 0;
-	if (c * c == a) {
-		b--;
-		ac++;
+	int ac2 = sqrt(a);
+	int ac3 = 0;
+	if (ac2 * ac2 == a) {
+		ac1--;
+		ac3++;
 	}
-	printf("%d \n", b);
-	for (int i = 1; i <= c; i++) {
+	printf("%d \n", ac1);
+	for (int i = 1; i <= ac2; i++) {
 		if (a%i == 0) {
 			printf("%d ", i);
 		}
 	}
-	for (int i = c - ac; i > 0; i--) {
+	for (int i = ac2 - ac3; i > 0; i--) {
 		if (a%i == 0) {
 			printf("%d ", a / i);
 		}
