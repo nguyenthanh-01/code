f46c4ddc3af170fda21c8b88d8aa3a342c7686b9
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 14:05:02 +0700

    Update bai028.c

--- c/chuong_01/bai028.c
+++ c/chuong_01/bai028.c
@@ -6,8 +6,8 @@ int main() {
     ll n, u1, d;
     scanf("%lld %lld %lld", &n, &u1, &d);
     ll un = u1 + (n - 1) * d;
-    ll s = n * (u1 + un) / 2;
-    printf("%lld \n", s);
+    ll ans = n * (u1 + un) / 2;
+    printf("%lld \n", ans);
     return 0;
 }
 
