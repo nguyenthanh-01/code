f8d924c8a871b1fcaacffc71d76af67b0d558324
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 13:54:40 +0700

    Update bai020.c

--- c/chuong_01/bai020.c
+++ c/chuong_01/bai020.c
@@ -7,7 +7,7 @@ int main() {
 	scanf("%lld %lld %lld", &a, &b, &c);
 	a += c - 1;
 	b += c - 1;
-	ll ac = (a / c) * (b / c);
-	printf("%lld \n", ac);
+	ll ans = (a / c) * (b / c);
+	printf("%lld \n", ans);
     return 0;
 }
