0c6696c311e5960db9f308f3cb2133cbb61f00a7
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 14:15:46 +0700

    Update bai004.c

--- c/chuong_02/bai004.c
+++ c/chuong_02/bai004.c
@@ -3,12 +3,12 @@
 typedef long long ll;
 
 int main() {
-    int a;
-    scanf("%d", &a);
-    double ac = 0;
-    for (int i = 1; i <= a; i++) {
-    	ac += (1.0 / i);
-	}
-	printf("%.3lf \n", ac);
-    return 0;
+  int a;
+  scanf("%d", &a);
+  double res = 0;
+  for (int i = 1; i <= a; i++) {
+    res += (1.0 / i);
+  }
+	printf("%.3lf \n", res);
+  return 0;
 }
