ab972ca69e8a342866d5f9567f1e8b41c0fa38af
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 15:36:41 +0700

    Create bai010.c

---
+++ c/chuong_02/bai010.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define yes printf("YES \n")
+#define no printf("NO \n")
+
+typedef long long ll;
+
+int main() {
+	int a;
+	scanf("%d", &a);
+	int flag = 0;
+	for (int i = 0; i < a; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		if (tmp == 2022) {
+			flag++;
+		}
+	}
+	flag ? yes : no;
+	return 0;
+}
