08a9524143228f7890d87d2405bc059abe434ef5
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 14:20:58 +0700

    Update bai005.c

--- c/chuong_02/bai005.c
+++ c/chuong_02/bai005.c
@@ -6,10 +6,10 @@ int main() {
 	int a;
 	scanf("%d", &a);
 	a *= 2;
-	double ac = 0;
+	double res = 0;
 	for (int i = 2; i <= a; i += 2) {
-		ac += (1.0 / i);
+		res += (1.0 / i);
 	}
-	printf("%.5lf \n", ac);
+	printf("%.5lf \n", res);
 	return 0;
 }
