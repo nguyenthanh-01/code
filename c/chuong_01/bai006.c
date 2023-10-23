3b76002c753ab8bc8d13a7803feda94436993836
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 13:46:59 +0700

    Update bai006.c

--- c/chuong_01/bai006.c
+++ c/chuong_01/bai006.c
@@ -14,10 +14,10 @@ int main() {
 	(a%3 == 0 || a%7 == 0) ? yes : no;
 	(a > 30 && a < 50) ? yes : no;
 	(a >= 30 && (a%2 == 0 || a%3 == 0 || a%5 == 0)) ? yes : no;
-	int ac = a%10;
-	(a >= 10 && a <= 99 && (ac == 2 || ac == 3 || ac == 5 || ac == 7)) ? yes : no;
+	int res = a%10;
+	(a >= 10 && a <= 99 && (res == 2 || res == 3 || res == 5 || res == 7)) ? yes : no;
 	(a <= 100 && a%23 == 0) ? yes : no;
 	(a < 10 || a > 20) ? yes : no;
-	(ac%3 == 0) ? yes : no;
+	(res%3 == 0) ? yes : no;
     return 0;
 }
