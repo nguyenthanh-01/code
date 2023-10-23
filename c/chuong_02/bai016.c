dc3016bc8e0d1c35de6a1f955a20a0fdb910a6c8
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-23 23:00:33 +0700

    Update bai016.c

--- c/chuong_02/bai016.c
+++ c/chuong_02/bai016.c
@@ -3,13 +3,17 @@
 typedef long long ll;
 
 int main() {
-  ll a;
-  scanf("%lld", &a);
-  int cnt = 0;
-  while (a) {
-    cnt++;
-    a /= 10;
-  }
-  printf("%d", cnt);
-  return 0;
+	ll a;
+	scanf("%lld", &a);
+	if (a == 0) {
+		printf("1 \n");
+		return 0;
+	}
+	int cnt = 0;
+	while (a) {
+		cnt++;
+		a /= 10;
+	}
+	printf("%d", cnt);
+	return 0;
 }
