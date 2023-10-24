01202b2cc7d69f812851a6375ea10614bf8504cb
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:24:12 +0700

    Update bai021.c

--- c/chuong_02/bai021.c
+++ c/chuong_02/bai021.c
@@ -7,45 +7,45 @@ int main() {
     scanf("%d", &a);
     
     for (int i = 0; i < a; i++) {
-    	for (int j = 0; j < a; j++) {
-    		printf("*");
-		}
-		printf("\n");
-	}
-	printf("\n");
-	
+        for (int j = 0; j < a; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
     for (int i = 0; i < a; i++) {
-    	for (int j = 0; j < a; j++) {
-    		if (i == 0 || j == 0 || j == a - 1 || i == a - 1) {
-    			printf("*");
-			}
-			else printf(" ");
-		}
-		printf("\n");
-	}
-	printf("\n");
-	
+        for (int j = 0; j < a; j++) {
+            if (i == 0 || j == 0 || j == a - 1 || i == a - 1) {
+                printf("*");
+            }
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
     for (int i = 0; i < a; i++) {
-    	for (int j = 0; j < a; j++) {
-    		if (i == 0 || j == 0 || j == a - 1 || i == a - 1) {
-    			printf("*");
-			}
-			else printf("#");
-		}
-		printf("\n");
-	}
-	printf("\n");
-	
+        for (int j = 0; j < a; j++) {
+            if (i == 0 || j == 0 || j == a - 1 || i == a - 1) {
+                printf("*");
+            }
+            else printf("#");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
     for (int i = 0; i < a; i++) {
-    	for (int j = 0; j < a; j++) {
-    		if (i == 0 || j == 0 || j == a - 1 || i == a - 1) {
-    			printf("%d ", i + 1);
-			}
-			else printf("  ");
-		}
-		printf("\n");
-	}
-	printf("\n");
-	
+        for (int j = 0; j < a; j++) {
+            if (i == 0 || j == 0 || j == a - 1 || i == a - 1) {
+                printf("%d ", i + 1);
+            }
+            else printf("  ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
     return 0;
 }
