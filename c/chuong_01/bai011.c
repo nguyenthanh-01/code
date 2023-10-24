1786155633def9603b88e12b2a1017fd44c0a5a4
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:07:51 +0700

    Update bai011.c

--- c/chuong_01/bai011.c
+++ c/chuong_01/bai011.c
@@ -8,17 +8,17 @@ int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
     if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a) {
-    	if (a == b && b == c) {
-    		printf("1 \n");
-		}
-		else if (a == b || a == c || b == c) {
-			printf("2 \n");
-		}
-		else if (ck(a, b, c) || ck(b, a, c) || ck(c, a, b)) {
-			printf("3 \n");
-		}
-		else printf("4 \n");
-	}
+        if (a == b && b == c) {
+            printf("1 \n");
+        }
+        else if (a == b || a == c || b == c) {
+            printf("2 \n");
+        }
+        else if (ck(a, b, c) || ck(b, a, c) || ck(c, a, b)) {
+            printf("3 \n");
+        }
+        else printf("4 \n");
+    }
     else printf("INVALID \n");
     return 0;
 }
