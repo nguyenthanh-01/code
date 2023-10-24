c110147234207daa9bd4d8c1fe501f56d10a8891
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:10:03 +0700

    Update bai014.c

--- c/chuong_01/bai014.c
+++ c/chuong_01/bai014.c
@@ -3,18 +3,18 @@
 typedef long long ll;
 
 int main() {
-	float a, b, c, d;
-	scanf("%f %f %f %f", &a, &b, &c, &d);
-	float tmp = (a + b + c * 2 + d * 3) / 7;
-	if (tmp < 5) {
-		printf("YEU \n");
-	}
-	else if (tmp < 6.5) {
-		printf("TRUNG BINH \n");
-	}
-	else if (tmp < 8) {
-		printf("KHA \n");
-	}
-	else printf("GIOI \n");
+    float a, b, c, d;
+    scanf("%f %f %f %f", &a, &b, &c, &d);
+    float tmp = (a + b + c * 2 + d * 3) / 7;
+    if (tmp < 5) {
+        printf("YEU \n");
+    }
+    else if (tmp < 6.5) {
+        printf("TRUNG BINH \n");
+    }
+    else if (tmp < 8) {
+        printf("KHA \n");
+    }
+    else printf("GIOI \n");
     return 0;
 }
