8f087863eabdcdacb6f6afb911b3309fc923c47a
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:21:03 +0700

    Update bai015.c

--- c/chuong_02/bai015.c
+++ c/chuong_02/bai015.c
@@ -3,12 +3,12 @@
 typedef long long ll;
 
 int main() {
-	int a;
-	scanf("%d", &a);
-	ll res = 1;
-	for (int i = 1; i <= a; i++) {
-		res *= i;
-	}
-	printf("%lld \n", res);
-	return 0;
+    int a;
+    scanf("%d", &a);
+    ll res = 1;
+    for (int i = 1; i <= a; i++) {
+        res *= i;
+    }
+    printf("%lld \n", res);
+    return 0;
 }
