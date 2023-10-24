8b43613f5016ec60eb1e314eeacb947faaa7e263
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:13:48 +0700

    Update bai018.c

--- c/chuong_01/bai018.c
+++ c/chuong_01/bai018.c
@@ -5,10 +5,10 @@
 typedef long long ll;
 
 int main() {
-	char a;
-	scanf("%c", &a);
-	if (a <= 'Z') (a < 'A') ? xuat(a) : xuat(a + 32);
-	else if (a <= 'z') (a < 'a') ? xuat(a) : xuat(a - 32);
-	else xuat(a);
+    char a;
+    scanf("%c", &a);
+    if (a <= 'Z') (a < 'A') ? xuat(a) : xuat(a + 32);
+    else if (a <= 'z') (a < 'a') ? xuat(a) : xuat(a - 32);
+    else xuat(a);
     return 0;
 }
