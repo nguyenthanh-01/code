2290d5cc73c1c95d90fc991382b57d035c89b0f4
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:21:58 +0700

    Update bai017.c

--- c/chuong_02/bai017.c
+++ c/chuong_02/bai017.c
@@ -3,13 +3,13 @@
 typedef long long ll;
 
 int main() {
-	ll a;
-	scanf("%lld", &a);
-	int res = 0;
-	while (a) {
-	    res += a%10;
-	    a /= 10;
-	}
-	printf("%d", res);
-	return 0;
+    ll a;
+    scanf("%lld", &a);
+    int res = 0;
+    while (a) {
+        res += a%10;
+        a /= 10;
+    }
+    printf("%d", res);
+    return 0;
 }
