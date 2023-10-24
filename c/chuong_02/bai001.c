20d929eb771fce444f9015a2ccb857da26a1cf01
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:15:53 +0700

    Update bai001.c

--- c/chuong_02/bai001.c
+++ c/chuong_02/bai001.c
@@ -3,11 +3,11 @@
 typedef long long ll;
 
 int main() {
-	int n;
-	scanf("%d", &n);
-	ll ans = 1ll * n * (n + 1) / 2;
-	printf("%lld \n", ans);
-	return 0;
+    int n;
+    scanf("%d", &n);
+    ll ans = 1ll * n * (n + 1) / 2;
+    printf("%lld \n", ans);
+    return 0;
 }
 
 /*
