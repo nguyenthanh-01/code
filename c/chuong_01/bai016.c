cec4a60e761365dacadcc2b962263a6512c90685
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:11:08 +0700

    Update bai016.c

--- c/chuong_01/bai016.c
+++ c/chuong_01/bai016.c
@@ -3,15 +3,15 @@
 typedef long long ll;
 
 int main() {
-	char a;
-	scanf("%c", &a);
-	if (a == 'z' || a == 'Z') {
-		printf("a \n");
-	}
-	else if (a < 'a') {
-		a += 32 + 1;
-		printf("%c \n", a);
-	}
-	else printf("%c \n", ++a);
+    char a;
+    scanf("%c", &a);
+    if (a == 'z' || a == 'Z') {
+        printf("a \n");
+    }
+    else if (a < 'a') {
+        a += 32 + 1;
+        printf("%c \n", a);
+    }
+    else printf("%c \n", ++a);
     return 0;
 }
