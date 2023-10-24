7c04d694d00e718b2daf277bface66d0fad865e6
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:23:42 +0700

    Update bai020.c

--- c/chuong_02/bai020.c
+++ c/chuong_02/bai020.c
@@ -3,18 +3,18 @@
 typedef long long ll;
 
 int main() {
-	int a;
-	scanf("%d", &a);
-	if (a == 1) {
-		printf("-1 \n");
-		return 0;
-	}
-	printf("%d \n", a / 2);
-	int tmp = a / 2 - a % 2;
-	for (int i = 0; i < tmp; i++) {
-		printf("2 ");
-	}
-	if (a%2) printf("3 ");
-	printf("\n");
-	return 0;
+    int a;
+    scanf("%d", &a);
+    if (a == 1) {
+        printf("-1 \n");
+        return 0;
+    }
+    printf("%d \n", a / 2);
+    int tmp = a / 2 - a % 2;
+    for (int i = 0; i < tmp; i++) {
+        printf("2 ");
+    }
+    if (a%2) printf("3 ");
+    printf("\n");
+    return 0;
 }
