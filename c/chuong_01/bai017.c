652f07eb71dcac19c8d7cac5f47a327db4f440c3
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:13:14 +0700

    Update bai017.c

--- c/chuong_01/bai017.c
+++ c/chuong_01/bai017.c
@@ -8,11 +8,11 @@
 typedef long long ll;
 
 int main() {
-	char a;
-	scanf("%c", &a);
-	if (a <= '9') (a < '0') ? sp : dg;
-	else if (a <= 'Z') (a < 'A') ? sp : up;
-	else if (a <= 'z') (a < 'a') ? sp : lw;
-	else sp;
-	return 0;
+    char a;
+    scanf("%c", &a);
+    if (a <= '9') (a < '0') ? sp : dg;
+    else if (a <= 'Z') (a < 'A') ? sp : up;
+    else if (a <= 'z') (a < 'a') ? sp : lw;
+    else sp;
+    return 0;
 }
