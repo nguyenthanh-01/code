fe22763b357b01b5576cca6b5467d4e3d0fa349e
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:09:25 +0700

    Update bai013.c

--- c/chuong_01/bai013.c
+++ c/chuong_01/bai013.c
@@ -3,12 +3,12 @@
 typedef long long ll;
 
 int main() {
-	int a;
-	scanf("%d", &a);
-	int b = a / 365;
-	a %= 365;
-	int c = a / 7;
-	a %= 7;
-	printf("%d %d %d \n", b, c, a);
+    int a;
+    scanf("%d", &a);
+    int b = a / 365;
+    a %= 365;
+    int c = a / 7;
+    a %= 7;
+    printf("%d %d %d \n", b, c, a);
     return 0;
 }
