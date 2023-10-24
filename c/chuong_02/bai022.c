097d3b6eb8ee1fd84e648126f221cef3a0647375
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:24:40 +0700

    Update bai022.c

--- c/chuong_02/bai022.c
+++ c/chuong_02/bai022.c
@@ -7,53 +7,53 @@ int main() {
     scanf("%d", &a);
     
     for (int i = 0; i < a; i++) {
-    	for (int j = 0; j <= i; j++) {
-    		printf("*");
-		}
-		printf("\n");
-	}
-	printf("\n");
-	
+        for (int j = 0; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
     for (int i = a; i > 0; i--) {
-    	for (int j = 0; j < i; j++) {
-    		printf("*");
-		}
-		printf("\n");
-	}
-	printf("\n");
-	
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
     for (int i = 0; i < a; i++) {
-    	for (int j = 0; j < a; j++) {
-    		if (j < a - 1 - i) {
-    			printf(" ");
-			}
-			else printf("*");
-		}
-		printf("\n");
-	}
-	printf("\n");
-	
+        for (int j = 0; j < a; j++) {
+            if (j < a - 1 - i) {
+                printf(" ");
+            }
+            else printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
     for (int i = 0; i < a; i++) {
-    	for (int j = 0; j < a; j++) {
-    		if (j > i - 1) {
-    			printf("*");
-			}
-			else printf(" ");
-		}
-		printf("\n");
-	}
-	printf("\n");
-	
+        for (int j = 0; j < a; j++) {
+            if (j > i - 1) {
+                printf("*");
+            }
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
     for (int i = 0; i < a; i++) {
-    	for (int j = 0; j <= i; j++) {
-    		if (i == 0 || j == 0 || i == a - 1 || j == i) {
-    			printf("*");
-			}
-			else printf(" ");
-		}
-		printf("\n");
-	}
-	printf("\n");
-	
+        for (int j = 0; j <= i; j++) {
+            if (i == 0 || j == 0 || i == a - 1 || j == i) {
+                printf("*");
+            }
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
     return 0;
 }
