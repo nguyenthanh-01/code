cb179fd0e5db5c4c9262e919e3651aa9057a375d
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 17:18:50 +0700

    Update bai007.c

--- c/chuong_02/bai007.c
+++ c/chuong_02/bai007.c
@@ -4,31 +4,31 @@
 typedef long long ll;
 
 int main() {
-	int a;
-	scanf("%d", &a);
-	int res = 0;
-	for (int i = 1; i * i <= a; i++) {
-		if (a%i == 0) {
-			res += 2;
-		}
-	}
-	int flag = 0;
-	int tmp = sqrt(a);
-	if (tmp * tmp == a) {
-		res -= 1;
-		flag = 1;
-	}
-	printf("%d \n", res);
-	for (int i = 1; i <= tmp; i++) {
-		if (a%i == 0) {
-			printf("%d ", i);
-		}
-	}
-	for (int i = tmp - flag; i > 0; i--) {
-		if (a%i == 0) {
-			printf("%d ", a / i);
-		}
-	}
-	printf("\n");
-	return 0;
+    int a;
+    scanf("%d", &a);
+    int res = 0;
+    for (int i = 1; i * i <= a; i++) {
+        if (a%i == 0) {
+            res += 2;
+        }
+    }
+    int flag = 0;
+    int tmp = sqrt(a);
+    if (tmp * tmp == a) {
+        res -= 1;
+        flag = 1;
+    }
+    printf("%d \n", res);
+    for (int i = 1; i <= tmp; i++) {
+        if (a%i == 0) {
+            printf("%d ", i);
+        }
+    }
+    for (int i = tmp - flag; i > 0; i--) {
+        if (a%i == 0) {
+            printf("%d ", a / i);
+        }
+    }
+    printf("\n");
+    return 0;
 }
