78775506e28780b898681c7ceadbda4eaac93fd4
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 09:50:19 +0700

    Update test.c

--- c/chuong_02/test.c
+++ c/chuong_02/test.c
@@ -20,5 +20,14 @@ int main() {
 		printf("\n");
 	}
 	printf("\n");
+
+	for (int i = 1; i <= a; i++) {
+		for (int j = 1; j <= a; j++) {
+			printf("%d ", j * i);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	
 	return 0;
 }
