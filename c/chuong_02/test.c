709ef11febccd3daf683d7bfa8dfb0986d18d58c
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 09:49:28 +0700

    Update test.c

--- c/chuong_02/test.c
+++ c/chuong_02/test.c
@@ -3,22 +3,22 @@
 typedef long long ll;
 
 int main() {
-    int a;
-    scanf("%d", &a);
-    for (int i = 1; i <= a; i++) {
-    	for (int j = 1; j <= a; j++) {
-    		printf("%d ", j * i);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	
+	int a;
+	scanf("%d", &a);
 	for (int i = 1; i <= a; i++) {
-    	for (int j = 1; j <= a; j++) {
-    		printf("%d ", j * i);
+		for (int j = 1; j <= a; j++) {
+			printf("%d ", j * i);
 		}
 		printf("\n");
 	}
 	printf("\n");
-    return 0;
+		
+	for (int i = 1; i <= a; i++) {
+		for (int j = 1; j <= a; j++) {
+			printf("%d ", j * i);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	return 0;
 }
