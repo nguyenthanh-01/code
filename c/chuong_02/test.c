3edf7a4f02e01cc922cfcf0a792cc720bcefdfa0
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-24 09:45:48 +0700

    Add files via upload

---
+++ c/chuong_02/test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef long long ll;
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    for (int i = 1; i <= a; i++) {
+    	for (int j = 1; j <= a; j++) {
+    		printf("%d ", j * i);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	
+	for (int i = 1; i <= a; i++) {
+    	for (int j = 1; j <= a; j++) {
+    		printf("%d ", j * i);
+		}
+		printf("\n");
+	}
+	printf("\n");
+    return 0;
+}
