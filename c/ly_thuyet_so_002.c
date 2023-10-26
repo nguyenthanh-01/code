4f02c17e7bcf3e758b8933cc2f534ccdbaf2763f
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-27 04:00:55 +0700

    Update ly_thuyet_so_002.c

--- c/ly_thuyet_so_002.c
+++ c/ly_thuyet_so_002.c
@@ -5,6 +5,9 @@ typedef long long ll;
 void ts_nt (int);
 int ucln (int, int);
 int bcnn (int, int);
+int tn (int);
+ll th (int, int);
+ll gt (int, int);
 
 int main() {
     int a, b;
@@ -12,7 +15,8 @@ int main() {
     printf("1. thua so nguyen to: "); ts_nt(a);
     printf("2. uoc chung lon nhat: %d \n", ucln(a, b));
     printf("2. boi chung nho nhat: %d \n", bcnn(a, b));
-    printf("3. so thuan nghich");
+    printf("3. so thuan nghich: %d \n", tn(a));
+    printf("4. to hop C(k, n): %lld \n", th(a, b));
     return 0;
 }
 
@@ -39,4 +43,27 @@ int ucln (int a, int b) {
 
 int bcnn (int a, int b) {
     return a / ucln(a, b) * b;
+}
+
+int tn (int a) {
+    int tmp = a;
+    int res = 0;
+    while (tmp) {
+        res = res * 10 + tmp % 10;
+        tmp /= 10;
+    }
+    return (res == a) ? 1 : 0;
+}
+
+ll th (int a, int b) {
+    int tmp = (a < b - a) ? a : b - a;
+    return gt(b, b - tmp) / gt(tmp, 1);
+}
+
+ll gt (int a, int b) {
+    ll res = 1;
+    for (int i = a; i > b; i--) {
+        res *= i;
+    }
+    return res;
 }
