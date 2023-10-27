8312017e92f690c4f8fc7e567fce0a068960c624
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-10-27 23:29:59 +0700

    Update bai007.c

--- c/chuong_02/bai007.c
+++ c/chuong_02/bai007.c
@@ -6,16 +6,16 @@ typedef long long ll;
 int main() {
     int a;
     scanf("%d", &a);
+    int tmp = sqrt(a);
     int res = 0;
-    for (int i = 1; i * i <= a; i++) {
+    for (int i = 1; i < tmp; i++) {
         if (a%i == 0) {
             res += 2;
         }
     }
     int flag = 0;
-    int tmp = sqrt(a);
-    if (tmp * tmp == a) {
-        res -= 1;
+    if (a%tmp == 0) {
+        res += 1;
         flag = 1;
     }
     printf("%d \n", res);
