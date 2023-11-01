a62e5a1be365c183e149c1c0dba8a880624e7ce4
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-01 09:19:25 +0700

    Update bai001.c

--- c/chuong_03/bai001.c
+++ c/chuong_03/bai001.c
@@ -1,1 +1,29 @@
-.
+#include <stdio.h>
+#include <math.h>
+
+#define yes printf("YES \n");
+#define no printf("NO \n");
+
+typedef long long ll;
+
+int nt (int);
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    if (nt(a)) {
+        yes;
+    }
+    else no;
+    return 0;
+}
+
+int nt (int a) {
+    int tmp = sqrt(a);
+    for (int i = 2; i <= tmp; i++) {
+        if (a%i == 0) {
+            return 0;
+        }
+    }
+    return a > 1;
+}
