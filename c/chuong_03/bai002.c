3a721bc8dd81c77e1db6e34795e331e8e3c87b85
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-01 15:19:27 +0700

    Create bai002.c

---
+++ c/chuong_03/bai002.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef long long ll;
+
+int sphenic(ll);
+
+int main() {
+    ll a;
+    scanf("%lld", &a);
+    if (sphenic(a)) {
+        printf("1");
+    }
+    else printf("0");
+    return 0;
+}
+
+int sphenic(ll a) {
+    int cnt = 0;
+    for (int i = 2; i <= (int) sqrt(a); i++) {
+        int cnt1 = 0;
+        while (a%i == 0) {
+            cnt1++;
+            cnt++;
+            a /= i;
+        }
+        if (cnt1 > 1) return 0;
+    }
+    if (a > 1) cnt++;
+    return cnt == 3;
+}
