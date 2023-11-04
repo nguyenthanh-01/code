ccb4252e9f326393d3a25b8477a599d09dd27143
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-05 03:13:01 +0700

    Create bai010.c

---
+++ c/chuong_03/bai010.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+#define yes printf("YES \n")
+#define no printf("NO \n")
+
+typedef long long ll;
+
+int cp (ll);
+
+int main() {
+    ll a;
+    scanf("%lld", &a);
+    if (cp(a)) {
+    	yes;
+	}
+	else no;
+    return 0;
+}
+
+int cp (ll a) {
+    int tmp = sqrt(a);
+    return (1ll * tmp * tmp == a) ? 1 : 0;
+}
