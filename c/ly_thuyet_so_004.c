1e8af9f8ed989ed4a39357edae4ac2d63ec0ed26
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-05 02:48:26 +0700

    Create ly_thuyet_so_004.c

---
+++ c/ly_thuyet_so_004.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+
+#define yes printf("YES \n")
+#define no printf("NO \n")
+
+typedef long long ll;
+
+int nt (ll);
+void ts_nt (ll);
+
+int main() {
+    ll a;
+    scanf("%lld", &a);
+    if (nt(a)) {
+        yes;
+    }
+    else no;
+    ts_nt(a);
+    return 0;
+}
+
+int nt (ll a) {
+    if (a < 2) return 0;
+    if (a == 2) return 1;
+    if (a%2 == 0) return 0;
+    int tmp = sqrt(a);
+    for (int i = 3; i <= tmp; i += 2) {
+        if (a%i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void ts_nt (ll a) {
+    if (a < 2) return;
+    while (a%2 == 0) {
+        printf("2 ");
+        a /= 2;
+    }
+    ll i = 3;
+    while (i * i <= a) {
+        while (a%i == 0) {
+            printf("%lld ", i);
+            a /= i;
+        }
+        i += 2;
+    }
+    if (a > 1) printf("%lld ", a);
+    printf("\n");
+}
