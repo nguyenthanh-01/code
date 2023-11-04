b0c66387008c4a021a8823ae58dca0ee6f15c00b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-04 09:36:46 +0700

    Update bai006.c

--- c/chuong_03/bai006.c
+++ c/chuong_03/bai006.c
@@ -15,9 +15,9 @@ ll tu (ll a) {
     int tmp = sqrt(a);
     ll res = 0;
     for (int i = 1; i <= tmp; i++) {
-    if (a%i == 0) {
-        res += (i + a / i);
-    }
+        if (a%i == 0) {
+            res += (i + a / i);
+        }
     }
     return (1ll * tmp * tmp == a) ? res - tmp : res;
 }
