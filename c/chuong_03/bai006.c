d531498469e46d187ecdb999f98ad4d0ea7fbbc9
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-04 09:35:20 +0700

    Create bai006.c

---
+++ c/chuong_03/bai006.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef long long ll;
+
+ll tu (ll);
+
+int main() {
+    ll a;
+    scanf("%lld", &a);
+    printf("%lld \n", tu(a));
+    return 0;
+}
+
+ll tu (ll a) {
+    int tmp = sqrt(a);
+    ll res = 0;
+    for (int i = 1; i <= tmp; i++) {
+    if (a%i == 0) {
+        res += (i + a / i);
+    }
+    }
+    return (1ll * tmp * tmp == a) ? res - tmp : res;
+}
