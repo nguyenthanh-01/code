2c935bd8f02127a2c3704a8393f0f00207133996
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-12 21:18:28 +0700

    Update sang_ng_to_doan.c

--- c/sang_ng_to_doan.c
+++ c/sang_ng_to_doan.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 
-#define n (int) 1e5
 #define max(a, b) (a > b) ? (a) : (b)
+#define n (int) 1e7
+int snt[n];
 
 typedef long long ll;
 
-void sang_nguyen_to(int [], ll, ll);
-void xuat(int [], ll, ll);
+void sang_nguyen_to(ll, ll);
+void xuat(ll, ll);
 
 int main() {
-    int a[n] = {0};
     ll l, r;
     scanf("%lld %lld", &l, &r);
-    sang_nguyen_to(a, l, r);
-    xuat(a, l, r);
+    sang_nguyen_to(l, r);
+    xuat(l, r);
     return 0;
 }
 
-void sang_nguyen_to(int a[], ll l, ll r) {
+void sang_nguyen_to(ll l, ll r) {
     for (ll i = l + (l%2 == 0); i <= r; i += 2) {
-        a[i - l] = 1;
+        snt[i - l] = 1;
     }
     for (ll i = 3; i * i <= r; i += 2) {
         for (ll j = max(i * i, (l + i - 1) / i * i); j <= r; j += i) {
-            a[j - l] = 0;
+            snt[j - l] = 0;
         }
     }
 }
 
-void xuat(int a[], ll l, ll r) {
+void xuat(ll l, ll r) {
     if (l < 3) printf("2 ");
     for (ll i = max(3ll, l + (l%2 == 0)); i <= r; i += 2) {
-        if (a[i - l]) {
+        if (snt[i - l]) {
             printf("%lld ", i);
         }
     }
