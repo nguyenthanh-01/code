f0c282896458ca5bb463bdd810be8b7d4fcede75
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-14 16:51:06 +0700

    Update nhi_phan_tich.c

--- c/nhi_phan_tich.c
+++ c/nhi_phan_tich.c
@@ -2,6 +2,8 @@
 
 typedef long long ll;
 
+const int mod = (int) 1e9+7;
+
 ll tich_np(ll, ll);
 
 int main() {
@@ -12,12 +14,15 @@ int main() {
 }
 
 ll tich_np(ll a, ll b) {
+    a %= mod;
     ll res = 0;
     while (b) {
         if (b%2) {
             res += a;
+            res %= mod;
         }
         a *= 2;
+        a %= mod;
         b /= 2;
     }
     return res;
