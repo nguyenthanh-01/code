e642d1b42e7b8d8ebbc1273d7c9e47895603cb35
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-16 00:31:14 +0700

    Update ly_thuyet_so_003.c

--- c/ly_thuyet_so_003.c
+++ c/ly_thuyet_so_003.c
@@ -4,7 +4,7 @@ typedef long long ll;
 
 int ck_fi (ll);
 void xuat_fi(ll);
-ll fi_n(ll);
+ll fi_n(int);
 
 int main() {
     ll a;
@@ -50,13 +50,14 @@ void xuat_fi(ll a) {
     printf("\n");
 }
 
-ll fi_n(ll a) {
+ll fi_n(int a) {
     if (a == 1) return 0;
     if (a == 2) return 1;
     ll f0 = 0;
     ll f1 = 1;
+    ll tmp = 0;
     for (int i = 2; i < a; i++) {
-        ll tmp = f0 + f1;
+        tmp = f0 + f1;
         f0 = f1;
         f1 = tmp;
     }
