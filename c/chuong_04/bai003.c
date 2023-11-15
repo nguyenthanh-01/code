660c69826f2670dff83e374122106c65e9e4c427
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-16 00:21:20 +0700

    Update bai003.c

--- c/chuong_04/bai003.c
+++ c/chuong_04/bai003.c
@@ -5,17 +5,17 @@ typedef long long ll;
 ll tong (int);
 
 int main() {
-  int a;
-  scanf("%d", &a);
-  printf("%lld \n", tong(a));
-  return 0;
+    int a;
+    scanf("%d", &a);
+    printf("%lld \n", tong(a));
+    return 0;
 }
 
 ll tong (int a) {
-  ll res = 0;
-  while (a) {
-    res += (1ll * a * a * a);
-    a--;
-  }
-  return res;
+    ll res = 0;
+    while (a) {
+        res += (1ll * a * a * a);
+        a--;
+    }
+    return res;
 }
