1d179c7b07d8991efc8dd79145e4f28e69c638f9
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-16 01:09:00 +0700

    Create bai009.c

---
+++ c/chuong_04/bai009.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef long long ll;
+
+const int mod = (int) 1e9 + 7;
+
+ll lt_np(ll, ll);
+
+int main() {
+    ll a; ll b;
+    scanf("%lld %lld", &a, &b);
+    printf("%lld \n", lt_np(a, b));
+    return 0;
+}
+
+ll lt_np(ll a, ll b) {
+    a %= mod;
+    ll res = 1;
+    while (b) {
+        if (b%2) {
+            res *= a;
+            res %= mod;
+        }
+        a *= a;
+        a %= mod;
+        b /= 2;
+    }
+    return res;
+}
+ 
