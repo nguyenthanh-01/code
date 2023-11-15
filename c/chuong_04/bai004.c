d2e620a96ebac4c091def91a345d4e8baa5047db
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-15 13:44:16 +0700

    Create bai004.c

---
+++ c/chuong_04/bai004.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int tong (int);
+
+int main() {
+  int a;
+  scanf("%d", &a);
+  printf("%d \n", tong(a));
+  return 0;
+}
+
+int tong (int a) {
+  int res = 0;
+  while (a) {
+    if (a%2) {
+      res -= a;
+    }
+    else res += a;
+    a--;
+  }
+  return res;
+}
