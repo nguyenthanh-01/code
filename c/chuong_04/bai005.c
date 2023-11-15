db8e86e733f0bda9b7c38a60031bc9fd4c36c1a9
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-15 14:12:44 +0700

    Create bai005.c

---
+++ c/chuong_04/bai005.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int tich (int);
+
+int main() {
+  int a;
+  scanf("%d", &a);
+  printf("%d \n", tich(a));
+  return 0;
+}
+
+int tich (int a) {
+  int res = 1;
+  while (a) {
+    res *= a;
+    a--;
+  }
+  return res;
+}
