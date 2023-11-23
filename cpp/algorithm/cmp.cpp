cbda18c4ceef733739239e62e40422e05069d54a
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-23 23:04:11 +0700

    Update cmp.cpp

--- cpp/algorithm/cmp.cpp
+++ cpp/algorithm/cmp.cpp
@@ -5,7 +5,14 @@ using namespace std;
 #define rall(a) (a).rbegin(), (a).rend()
 
 typedef long long ll;
+typedef struct {int a, b;} ii;
+typedef struct {int a, b, c;} iii;
 typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+
+inline bool cmp_ii (ii &i, ii &j) {return i.a < j.a;}
+inline bool cmp_iii (iii &i, iii &j) {return i.a < j.a;}
 
 int main() {
     return 0;
