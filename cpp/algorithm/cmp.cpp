cb8815d8ce59717439e7aa83899593a7f0af4df4
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-23 22:58:17 +0700

    Create cmp.cpp

---
+++ cpp/algorithm/cmp.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int main() {
+    return 0;
+}
