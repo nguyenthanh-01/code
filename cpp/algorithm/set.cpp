bd4e736b75915f63a0c57c598dbb6018facb6296
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-24 00:52:36 +0700

    Update set.cpp

--- cpp/algorithm/set.cpp
+++ cpp/algorithm/set.cpp
@@ -1,1 +1,20 @@
-.
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define su(a) (a).erase(unique(all(a)), (a).end())
+#define sz(a) (int) (a).size()
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int main() {
+    vi a = {2, 0, 8, 5, 1, 6, 0, 1, 4, 6};
+    sort(all(a));
+    su(a);
+    for (auto &i: a) {
+        cout << i << " ";
+    }
+    return 0;
+}
