dbe493a79b756b615b6fd0b4c83ecb678f3f14a6
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-24 00:00:26 +0700

    Create bin.cpp

---
+++ cpp/algorithm/bin.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define lb low_bound
+#define ub upper_bound
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int main() {
+    vi a = {2, 0, 8, 5, 1, 6, 0, 1, 4, 6};
+    
+    return 0;
+}
