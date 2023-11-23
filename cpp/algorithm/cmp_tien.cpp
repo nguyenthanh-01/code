c4d2f7eb6215e5138b0db728fe43037edd5800aa
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-23 23:34:55 +0700

    Create cmp_tien.cpp

---
+++ cpp/algorithm/cmp_tien.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+typedef long long ll;
+typedef struct {int a, b;} ii;
+typedef struct {int a, b, c;} iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+
+inline bool cmp_ii (ii &i, ii &j) {
+		if (i.a < j.a) return 1;
+		if (i.a == j.a) return (i.b < j.b) ? 1 : 0;
+		return 0;
+	}
+
+int main() {
+    vii a = {{3, 0}, {6, 5}, {0, 3}, {1, 3}, {7, 1}, {2, 1}, {7, 7}, {0, 2}, {3, 8}, {7, 4}};
+    sort(all(a), cmp_ii);
+    for (auto &i: a) {
+        cout << "[" << i.a << " " << i.b << "]" << " ";
+    }
+    return 0;
+}
