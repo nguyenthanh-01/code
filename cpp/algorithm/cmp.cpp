7c77879a34472d3150fdfec4f7e88ea8e85dc7ad
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-23 23:36:51 +0700

    Update cmp.cpp

--- cpp/algorithm/cmp.cpp
+++ cpp/algorithm/cmp.cpp
@@ -6,13 +6,10 @@ using namespace std;
 
 typedef long long ll;
 typedef struct {int a, b;} ii;
-typedef struct {int a, b, c;} iii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
-typedef vector<iii> viii;
 
-inline bool cmp_ii (ii &i, ii &j) {return i.a < j.a;}
-inline bool cmp_iii (iii &i, iii &j) {return i.a < j.a;}
+inline bool cmp (ii &i, ii &j) {return i.a < j.a;}
 
 int main() {
     return 0;
