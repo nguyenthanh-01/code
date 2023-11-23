7a9dcccd5dbbfdc0bb818ebbc71de5eee51c9ede
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-24 00:40:18 +0700

    Create set.cpp

---
+++ cpp/algorithm/set.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define su(a) (a).erase(unique(all(a), (a).end()))
+#define sz(a) (int) (a).size()
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int main() {
+    return 0;
+}
