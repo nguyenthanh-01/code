f13d0f083a6bafa88594eee36c0a6e47badfa604
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-25 22:56:56 +0700

    Update bai020.c

--- c/chuong_04/bai020.c
+++ c/chuong_04/bai020.c
@@ -9,7 +9,7 @@ int thao_tac (ll);
 int main() {
     ll a;
     scanf("%lld", &a);
-    (a == 1) ? printf("1 \n") : printf("%d \n", thao_tac(a));
+    printf("%d \n", thao_tac(a));
     return 0;
 }
 
@@ -18,6 +18,6 @@ int thao_tac (ll a) {
     int res1 = 1e9, res2 = 1e9, res3 = 1e9;
     if (a%3 == 0) res1 = thao_tac(a / 3) + 1;
     if (a%2 == 0) res2 = thao_tac(a / 2) + 1;
-    if (a > 1) res3 = thao_tac(a - 1) + 1;
+    res3 = thao_tac(a - 1) + 1;
     return min(res1, min(res2, res3));
 }
