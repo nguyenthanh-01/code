0d3697d384eea5815a00a5aa58bbed56f34b4e0e
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-11-25 22:54:35 +0700

    Update bai020.c

--- c/chuong_04/bai020.c
+++ c/chuong_04/bai020.c
@@ -18,6 +18,6 @@ int thao_tac (ll a) {
     int res1 = 1e9, res2 = 1e9, res3 = 1e9;
     if (a%3 == 0) res1 = thao_tac(a / 3) + 1;
     if (a%2 == 0) res2 = thao_tac(a / 2) + 1;
-    res3 = thao_tac(a - 1) + 1;
+    if (a > 1) res3 = thao_tac(a - 1) + 1;
     return min(res1, min(res2, res3));
 }
