d0eb1f78cfc786de0278e3c4301fbb0d72e76541
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-08 21:25:59 +0700

    Create de_quy_tien.cpp

---
+++ cpp/de_quy_tien.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int x = 0;
+
+void dq(int);
+
+int main() {
+    dq(5);
+    cout << x;
+    return 0;
+}
+
+void dq(int a) {
+    if (a == 0) return;
+    x += a;
+    dq(a - 1);
+}
