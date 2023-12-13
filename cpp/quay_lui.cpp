3ab4dd1eebf76808b0641a2bd00248b5bcf72a59
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-13 15:53:37 +0700

    Create quay_lui.cpp

---
+++ cpp/quay_lui.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ans = INT_MAX;
+int cnt = -1;
+
+void dq(int, int);
+
+int main() {
+    int a;
+    cin >> a;
+    dq(a, 0);
+    cout << ans << "\n";
+    cout << cnt << "\n";
+    return 0;
+}
+
+void dq(int a, int x) {
+    cnt++;
+    // if(ans < x) return;
+    if(a == 1) {
+        // ans = x;
+        if(ans > x) swap(ans, x);
+        return;
+    }
+    if(a%3 == 0) dq(a / 3, x + 1);
+    if(a%2 == 0) dq(a / 2, x + 1);
+    dq(a - 1, x + 1);
+}
+
+/*
+38
+5
+1614
+*/
