728aff8c3fc5fb043a128aee9de50e08e31e7184
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-13 15:26:13 +0700

    Update de_quy_nho.cpp

--- cpp/de_quy_nho.cpp
+++ cpp/de_quy_nho.cpp
@@ -1,16 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dq(int, int);
+int ans = INT_MAX;
+
+void dq(int, int);
 
 int main() {
-  int a;
-  cin >> a;
-  cout << dq(a, 0);
-  return 0;
+    int a;
+    cin >> a;
+    dq(a, 0);
+    cout << ans << "\n";
+    return 0;
 }
 
-int dq(int a, int x) {
-  if(a == 0) return x;
-  return dq(a - 1, x + a);
+void dq(int a, int x) {
+    if(x >= ans) return;
+    if(a == 1) {
+        ans = x;
+        return;
+    }
+    if(a%3 == 0) dq(a / 3, x + 1);
+    if(a%2 == 0) dq(a / 2, x + 1);
+    dq(a - 1, x + 1);
 }
