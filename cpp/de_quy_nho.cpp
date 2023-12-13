55015232d96452fb2c46d4d703f0d816c1f4f896
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-13 13:45:54 +0700

    Update de_quy_nho.cpp

--- cpp/de_quy_nho.cpp
+++ cpp/de_quy_nho.cpp
@@ -1,1 +1,16 @@
-.
+#include <bits/stdc++.h>
+using namespace std;
+
+int dq(int, int);
+
+int main() {
+  int a;
+  cin >> a;
+  cout << dq(a, 0);
+  return 0;
+}
+
+int dq(int a, int x) {
+  if(a == 0) return x;
+  return dq(a - 1, x + a);
+}
