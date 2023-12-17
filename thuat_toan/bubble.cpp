04c64b35e5cfddf01ce5e8f4f3bba23816d287ff
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-18 01:11:59 +0700

    Update and rename noi_bot.cpp to bubble.cpp

---
+++ thuat_toan/bubble.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pp pop_back
+
+typedef long long ll;
+typedef vector<int> vi;
+
+void bubble(vi &);
+
+int main() {
+	vi a = {1, 2, 3, 5, 4};
+	bubble(a);
+    return 0;
+}
+
+void bubble(vi &a) {
+	
+}
