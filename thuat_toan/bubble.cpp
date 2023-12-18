561f59bfe6958360c84917d1d5ee622abc0a682e
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-18 14:17:57 +0700

    Update bubble.cpp

--- thuat_toan/bubble.cpp
+++ thuat_toan/bubble.cpp
@@ -12,27 +12,31 @@ void bubble(vi &);
 void xuat(vi &);
 
 int main() {
-	vi a = {1, 2, 3, 5, 4};
-	bubble(a);
-	xuat(a);
+    vi a = {1, 2, 3, 5, 6};
+    bubble(a);
+    xuat(a);
     return 0;
 }
 
 void bubble(vi &a) {
-	int n = sz(a) - 1;
-	while(n > 1) {
-		for(int i = 0; i < n; i++) {
-			if(a[i] > a[i + 1]) {
-				int tmp = a[i];
-				a[i] = a[i + 1];
-				a[i + 1] = tmp;
-			}
-		}
-	}
+    int n = sz(a) - 1;
+    while(n) {
+        bool flag = true;
+        for(int i = 0; i < n; i++) {
+            if(a[i] > a[i + 1]) {
+                int tmp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = tmp;
+                flag = false;
+            }
+        }
+        if(flag) return;
+        n--;
+    }
 }
 
 void xuat(vi &a) {
-	for(int &i: a) {
-		cout << i << " ";
-	}
+    for(int &i: a) {
+        cout << i << " ";
+    }
 }
