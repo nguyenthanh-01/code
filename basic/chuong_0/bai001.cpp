dd55f06f63d8190deb0c8f50935dc3ef61cd0661
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-21 22:40:24 +0700

    Create bai001.cpp

---
+++ basic/chuong_0/bai001.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a; cin >> a;
+    cout << a << endl;
+    cout << "Hello World !" << endl;
+    cout << "28tech C++ programming !" << endl;
+    return 0;
+}
