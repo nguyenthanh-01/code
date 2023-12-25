7a1c53a20ed3ec15d58ef481d146a28f4431785b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-26 05:37:32 +0700

    Update bai009.py

--- python/chuong_01_string/bai009.py
+++ python/chuong_01_string/bai009.py
@@ -1,6 +1,6 @@
-# 
-text = 'Hello World'
+# chuyển đổi chữ hoa > chữ thường
+text = "Hello World"
 shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 index = alphabet.find(text[0])
-print(index)
+print(text.lower())
