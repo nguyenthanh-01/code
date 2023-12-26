c41d07c82c9cfce900fe346cf1e96c451e214e06
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-27 01:22:58 +0700

    Update bai013.py

--- python/chuong_01_string/bai013.py
+++ python/chuong_01_string/bai013.py
@@ -1,4 +1,4 @@
-#
+# nối chuỗi
 text = "Hello World"
 shift = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -7,4 +7,4 @@ for char in text.lower():
     index = alphabet.find(char)
     new_index = index + shift
     encrypted_text += alphabet[new_index]
-    print(f"char: {char} encrypted text: {encrypted_text}")
+print(f"encrypted text: {encrypted_text}")
