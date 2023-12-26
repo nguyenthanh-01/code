2477b978b06f22a446b2789f8e0c8d459974bcef
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-26 23:25:59 +0700

    Update bai011.py

--- python/chuong_01_string/bai011.py
+++ python/chuong_01_string/bai011.py
@@ -5,5 +5,5 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 for char in text:
     index = alphabet.find(char.lower())
     new_index = index + shift
-    shifted = alphabet[new_index]
-    print(shifted)
+    new_char = alphabet[new_index]
+    print(new_char)
