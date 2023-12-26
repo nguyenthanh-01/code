95a7ad51a165acfd4e2d012a7b3a1a81c032875d
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-26 16:39:51 +0700

    Update bai011.py

--- python/chuong_01_string/bai011.py
+++ python/chuong_01_string/bai011.py
@@ -2,7 +2,7 @@
 text = "Hello World"
 shift = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-for i in text:
-    index = alphabet.find(i.lower())
+for char in text:
+    index = alphabet.find(char.lower())
     shifted = alphabet[index + shift]
     print(shifted)
