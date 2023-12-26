6e5aa66331378430e5a985f39102262fe6d9d0e2
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-26 16:51:40 +0700

    Update bai012.py

--- python/chuong_01_string/bai012.py
+++ python/chuong_01_string/bai012.py
@@ -4,5 +4,6 @@ shift = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 for char in text.lower():
     index = alphabet.find(char)
-    shifted = alphabet[index + shift]
+    new_index = index + shift
+    shifted = alphabet[new_index]
     print(shifted)
