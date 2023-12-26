ba57afeb38afd20a75a5d2272d13fa2619cc2730
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-27 01:15:39 +0700

    Update bai012.py

--- python/chuong_01_string/bai012.py
+++ python/chuong_01_string/bai012.py
@@ -6,5 +6,5 @@ encrypted_text = ""
 for char in text.lower():
     index = alphabet.find(char)
     new_index = index + shift
-    encrypted_text = alphabet[new_index]
+    encrypted_text += alphabet[new_index]
     print(f"char: {char} encrypted text: {encrypted_text}")
