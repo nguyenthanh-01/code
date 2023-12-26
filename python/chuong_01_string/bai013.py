2e4c80e18590522bd17770894c68e4c8cdff5118
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-27 01:17:03 +0700

    Create bai013.py

---
+++ python/chuong_01_string/bai013.py
@@ -0,0 +1,10 @@
+#
+text = "Hello World"
+shift = 3
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+encrypted_text = ""
+for char in text.lower():
+    index = alphabet.find(char)
+    new_index = index + shift
+    encrypted_text += alphabet[new_index]
+    print(f"char: {char} encrypted text: {encrypted_text}")
