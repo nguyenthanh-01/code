25304045648061e365f45a048a046a3fe8c6fc28
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-29 22:12:55 +0700

    Create bai015.py

---
+++ python/chuong_01_string/bai015.py
@@ -0,0 +1,18 @@
+# hàm
+def main():
+    text = "Hello World"
+    custom_key = "python"
+    vigenere(text, custom_key)
+
+def vigenere(message, key):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    encrypted_text = ""
+    key_index = 0
+    for char in message.lower():
+        index = alphabet.find(char)
+        new_index = (index + key) % len(alphabet)
+        encrypted_text += alphabet[new_index]
+    print('plain text:', message)
+    print(f"encrypted text: {encrypted_text}")
+
+main()
