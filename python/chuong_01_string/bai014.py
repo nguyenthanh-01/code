0c23793fe2a0780c9470dcbf3d6db4f048b4460d
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-29 21:46:51 +0700

    Update bai014.py

--- python/chuong_01_string/bai014.py
+++ python/chuong_01_string/bai014.py
@@ -2,14 +2,14 @@
 def main():
     text = "Hello World"
     shift = 3
-    caesar(text, shift)
+    vigenere(text, shift)
 
-def caesar(message, offset):
+def vigenere(message, key):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     encrypted_text = ""
     for char in message.lower():
         index = alphabet.find(char)
-        new_index = (index + offset) % len(alphabet)
+        new_index = (index + key) % len(alphabet)
         encrypted_text += alphabet[new_index]
     print('plain text:', message)
     print(f"encrypted text: {encrypted_text}")
