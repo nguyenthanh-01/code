9eb0957c81045b756c958a9618c63447313cb5c4
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-30 22:46:31 +0700

    Create bai016.py

---
+++ python/chuong_01_string/bai016.py
@@ -0,0 +1,28 @@
+# trả về
+def main():
+    text = "Hello World"
+    custom_key = "python"
+    encryption = vigenere(text, custom_key)
+    print(encryption)
+
+def vigenere(message, key):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    encrypted_text = ""
+    key_index = 0
+    for char in message.lower():
+        # thêm ký tự space vào tin nhắn
+        if(char == " "):
+            encrypted_text += char
+            continue
+        # tìm ký tự key để mã hóa
+        else:
+            key_char = key[key_index % len(key)]
+            key_index += 1
+        # mã hóa
+        offset = alphabet.index(key_char)
+        index = alphabet.find(char)
+        new_index = (index + offset) % len(alphabet)
+        encrypted_text += alphabet[new_index]
+    return encrypted_text
+
+main()
