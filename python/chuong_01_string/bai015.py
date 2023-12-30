cf6c8901ba1ecb7e373ec7e42effc32ff1d02280
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2023-12-30 22:40:54 +0700

    Update bai015.py

--- python/chuong_01_string/bai015.py
+++ python/chuong_01_string/bai015.py
@@ -9,8 +9,18 @@ def vigenere(message, key):
     encrypted_text = ""
     key_index = 0
     for char in message.lower():
+        # thêm ký tự space vào tin nhắn
+        if(char == " "):
+            encrypted_text += char
+            continue
+        # tìm ký tự key để mã hóa
+        else:
+            key_char = key[key_index % len(key)]
+            key_index += 1
+        # mã hóa
+        offset = alphabet.index(key_char)
         index = alphabet.find(char)
-        new_index = (index + key) % len(alphabet)
+        new_index = (index + offset) % len(alphabet)
         encrypted_text += alphabet[new_index]
     print('plain text:', message)
     print(f"encrypted text: {encrypted_text}")
