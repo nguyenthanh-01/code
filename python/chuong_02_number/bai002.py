a4063039d480abc447badfbd67c15f42519b62d3
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-06 23:28:36 +0700

    Update bai002.py

--- python/chuong_02_number/bai002.py
+++ python/chuong_02_number/bai002.py
@@ -1,4 +1,5 @@
-# chuyển đổi ký tự
+# maketrans bảng chuyển đổi
+# translate chuyển đổi
 def main():
     card_number = "4111-1111-4555-1142"
     card_translation = str.maketrans({'-': ' ', ' ': ''})
