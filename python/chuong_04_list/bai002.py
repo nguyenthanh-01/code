b55b3f35160e8125a7ba297cb7eace130fda2c51
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-13 21:01:50 +0700

    Create bai002.py

---
+++ python/chuong_04_list/bai002.py
@@ -0,0 +1,17 @@
+# ''.join(snake_cased_char_list)
+# input: snake_cased_char_list
+# output: str
+
+def main():
+    pass
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = []
+    for char in pascal_or_camel_cased_string:
+        if char.isupper():
+            converted_character = '_' + char.lower()
+        else:
+            snake_cased_char_list.append(char)
+    snake_cased_string = ''.join(snake_cased_char_list)
+
+main()
