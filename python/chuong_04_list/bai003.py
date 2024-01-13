39e60c8e6e4281daf43a67eef51d0887924e575b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-13 21:10:16 +0700

    Create bai003.py

---
+++ python/chuong_04_list/bai003.py
@@ -0,0 +1,15 @@
+# strip('_'): xóa "_" ở 2 đầu chuỗi
+def main():
+    pass
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = []
+    for char in pascal_or_camel_cased_string:
+        if char.isupper():
+            converted_character = '_' + char.lower()
+        else:
+            snake_cased_char_list.append(char)
+    snake_cased_string = ''.join(snake_cased_char_list)
+    clean_snake_cased_string = snake_cased_string.strip('_')
+
+main()
