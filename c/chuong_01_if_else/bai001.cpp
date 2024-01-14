ca703ba23061b0c9e429b05b8df6ae8773d76040
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-14 21:42:22 +0700

    Update and rename bai001.py to bai001.cpp

---
+++ c/chuong_01_if_else/bai001.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int x; cin >> x;
+    ll res = 1ll * x * x * x + 3ll * x * x + x + 1;
+    cout << res;
+    return 0;
+}
