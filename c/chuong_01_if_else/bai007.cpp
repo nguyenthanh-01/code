01fe80f936347d65d6ad756c97a6d40181bc3bd7
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-15 02:56:37 +0700

    Create bai007.cpp

---
+++ c/chuong_01_if_else/bai007.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int x = a / b * b;
+    int y = (a%b == 0) ? a : (a / b + 1) * b;
+    cout << x << " " << y;
+    return 0;
+}
