71996bff1dfca3314b5ad27cba76a23649f73149
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-15 00:45:35 +0700

    Create bai004.cpp

---
+++ c/chuong_01_if_else/bai004.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int r; cin >> r;
+    double chuvi = 2 * 3.14 * r;
+    double dientich = 3.14 * r * r;
+    printf("%.4lf %.4lf", chuvi, dientich);
+    return 0;
+}
