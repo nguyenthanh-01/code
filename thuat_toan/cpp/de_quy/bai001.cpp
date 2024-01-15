b58c42e8126f5ff8fa4d99cd3f12e48c9bb711a5
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-15 20:44:22 +0700

    Update bai001.cpp

--- thuat_toan/cpp/de_quy/bai001.cpp
+++ thuat_toan/cpp/de_quy/bai001.cpp
@@ -7,14 +7,14 @@ void dq(int, int &);
 
 int main() {
     int n; cin >> n;
-    int x = 0;
+    int x = 0; // biến nhớ
     dq(n, x);
     cout << x << "\n";
     return 0;
 }
 
 void dq(int n, int &x) {
-    if (n == 0) return;
-    x += n;
-    dq(n - 1, x);
+    if (n == 0) return; // kết thúc
+    x += n; // code
+    dq(n - 1, x); // đệ quy
 }
