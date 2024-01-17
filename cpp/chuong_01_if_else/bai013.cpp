86d4c306e0a1365d08bc2e0797f145d30917510e
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-17 23:06:50 +0700

    Create bai013.cpp

---
+++ cpp/chuong_01_if_else/bai013.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n; cin >> n;
+    int nam = n / 365;
+    n %= 365;
+    int tuan = n / 7;
+    n %= 7;
+    cout << nam << " " << tuan << " " << n << "\n";
+    return 0;
+}
