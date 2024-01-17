0df7d59008cacaa0dc5ab83dec44455d317d93af
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-17 12:55:51 +0700

    Update bai004.cpp

--- cpp/chuong_01_if_else/bai004.cpp
+++ cpp/chuong_01_if_else/bai004.cpp
@@ -7,6 +7,6 @@ int main() {
     int r; cin >> r;
     double chuvi = 2 * 3.14 * r;
     double dientich = 3.14 * r * r;
-    printf("%.4lf %.4lf", chuvi, dientich);
+    printf("%.4lf %.4lf \n", chuvi, dientich);
     return 0;
 }
