f200b3777fb7afbfe706a025cc0b791f4d90b08a
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-17 12:50:42 +0700

    Create bai003.cpp

---
+++ cpp/chuong_01_if_else/bai003.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int c; cin >> c;
+    double f = (1.0 * c * 9 / 5) + 32;
+    printf("%.2lf", f);
+    return 0;
+}
