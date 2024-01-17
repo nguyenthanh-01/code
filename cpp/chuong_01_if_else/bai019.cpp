ba030d9ec9ed93414b35ad787e6fe4049b545151
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-17 23:51:11 +0700

    Create bai019.cpp

---
+++ cpp/chuong_01_if_else/bai019.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << 1ll * n / 2 * m + n % 2 * m / 2;
+    return 0;
+}
