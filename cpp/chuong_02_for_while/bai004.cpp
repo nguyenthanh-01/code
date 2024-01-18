1cc6a46f591f35a6670d8f629fafd04fa03fe6fb
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-18 14:06:28 +0700

    Create bai004.cpp

---
+++ cpp/chuong_02_for_while/bai004.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  int n; cin >> n;
+  double tong = 0;
+  for (int i = 1; i <= n; i++) {
+    tong += 1.0 / i;
+  }
+  cout << tong;
+  return 0;
+}
