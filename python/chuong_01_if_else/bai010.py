205fb928b751e0ba2ff8f54db0d832d5285128af
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-21 04:12:49 +0700

    Update bai010.py

--- python/chuong_01_if_else/bai010.py
+++ python/chuong_01_if_else/bai010.py
@@ -1,9 +1,9 @@
 def main():
     a, b, c = map(int, input().split())
-    if a > 0 and b > 0 and c > 0 and a + b > c and a + c > b and b + c > a:
-        print("YES")
-    else:
+    if a <= 0 or b <= 0 or c <= 0 or a + b <= c or a + c <= b or b + c <= a:
         print("NO")
+    else:
+        print("YES")
 
 if __name__ == "__main__":
     main()
