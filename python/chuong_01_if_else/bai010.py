c28c998c78b713b014d0effdacab5b8b9a28f68b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-21 04:10:19 +0700

    Create bai010.py

---
+++ python/chuong_01_if_else/bai010.py
@@ -0,0 +1,9 @@
+def main():
+    a, b, c = map(int, input().split())
+    if a > 0 and b > 0 and c > 0 and a + b > c and a + c > b and b + c > a:
+        print("YES")
+    else:
+        print("NO")
+
+if __name__ == "__main__":
+    main()
