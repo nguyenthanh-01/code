83a573538f90fba8a22da8cbf32991f93a59606a
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-22 00:33:24 +0700

    Create bai029.py

---
+++ python/chuong_01_if_else/bai029.py
@@ -0,0 +1,13 @@
+def main():
+    a, b, c, d = map(int, input().split())
+    if b%a == 0:
+        q = b // a
+        if b*q == c and c*q == d:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("NO")
+
+if __name__ == "__main__":
+    main()
