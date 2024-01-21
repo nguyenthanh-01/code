686948fbb4f0f81ff9da76b7360ee0609e33435c
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-22 00:49:02 +0700

    Update bai031.py

--- python/chuong_01_if_else/bai031.py
+++ python/chuong_01_if_else/bai031.py
@@ -4,11 +4,11 @@ def main():
     cup = a1 + a2 + a3
     hc = b1 + b2 + b3
     ke1, ke2 = 0, 0
-    if cup%5 == 0:
+    if cup % 5 == 0:
         ke1 = cup // 5
     else:
         ke1 = cup // 5 + 1
-    if hc%10 == 0:
+    if hc % 10 == 0:
         ke2 = hc // 10
     else:
         ke2 = hc // 10 + 1
