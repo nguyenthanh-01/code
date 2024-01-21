09fc95fc093ff96354f8353dc5446808357d38d7
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-21 23:49:56 +0700

    Create bai019.py

---
+++ python/chuong_01_if_else/bai019.py
@@ -0,0 +1,9 @@
+def main():
+    n, m = map(int, input().split())
+    if n%2 == 0:
+        print(n // 2 * m)
+    else:
+        print((n - 1) // 2 * m + m // 2)
+
+if __name__ == "__main__":
+    main()
