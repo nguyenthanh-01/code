919db8e4287a2d21005c90d65c8b4e553e64f309
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-22 00:42:23 +0700

    Create bai032.py

---
+++ python/chuong_01_if_else/bai032.py
@@ -0,0 +1,8 @@
+def main():
+    k2, k3, k5, k6 = map(int, input().split())
+    k256 = min(k2, k5, k6)
+    k32 = min(k3, k2 - k256)
+    print(k32 * 32 + k256 * 256)
+
+if __name__ == "__main__":
+    main()
