3915a0c275f469c416a9557c7218a6a6be809847
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-21 22:32:55 +0700

    Create bai014.py

---
+++ python/chuong_01_if_else/bai014.py
@@ -0,0 +1,14 @@
+def main():
+    a, b, c, d = map(float, input().split())
+    tb = (a + b + c * 2 + d * 3) / 7
+    if tb >= 8:
+        print("GIOI")
+    elif tb >= 6.5:
+        print("KHA")
+    elif tb >= 5:
+        print("TRUNG BINH")
+    else:
+        print("YEU")
+
+if __name__ == "__main__":
+    main()
