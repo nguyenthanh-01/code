6c5e52a0a7375d10634daa098602877d3a86c3ca
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-22 01:16:29 +0700

    Update bai033.py

--- python/chuong_01_if_else/bai033.py
+++ python/chuong_01_if_else/bai033.py
@@ -2,10 +2,10 @@ def main():
     a, b, c, n = map(int, input().split())
     if (n + a + b + c) % 3 == 0:
         tmp = (n + a + b + c) // 3
-        if tmp >= a and tmp >= b and tmp >= c:
-            print("YES")
-        else:
+        if tmp < a or tmp < b or tmp < c:
             print("NO")
+        else:
+            print("YES")
     else:
         print("NO")
 
