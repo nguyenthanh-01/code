7d07caedde3cfb5d6994050cb77223760e72643b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-22 01:13:43 +0700

    Update bai005.py

--- python/chuong_01_if_else/bai005.py
+++ python/chuong_01_if_else/bai005.py
@@ -1,6 +1,6 @@
 def main():
     x1, y1, x2, y2 = map(int, input().split())
-    res = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+    res = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
     print(f"{res:.2f}")
 
 if __name__ == "__main__":
