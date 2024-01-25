51b008b16069e7500a0b8b03c2eddc186d5eb9a8
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-25 22:32:31 +0700

    Create bai008.cpp

---
+++ cpp/bai_tap/chuong01_if_else/bai008.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+typedef long long ll;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int tong = a + b;
+    int hieu = a - b;
+    ll tich = 1ll * a * b;
+    cout << tong << endl << hieu << endl << tich << endl;
+    if(b == 0) {
+        cout << "INVALID\n";
+    }
+    else {
+        double thuong = 1.0 * a / b;
+        printf("%.4lf \n", thuong);
+    }
+    return 0;
+}
