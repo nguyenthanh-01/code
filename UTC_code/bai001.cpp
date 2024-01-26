3c9fae0723bf65734c3a23d194d5663fedc336a7
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-26 21:27:49 +0700

    Create bai001.cpp

---
+++ UTC_code/bai001.cpp
@@ -0,0 +1,44 @@
+# 002bai
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define sl (int) 1e5+1
+#define sz(a) (int) (a).size()
+
+typedef long long ll;
+typedef vector<int> vi;
+
+void sang_nt(vi &);
+
+int main() {
+    vi sg(sl, 1); sg[0] = 0;
+    sang_nt(sg);
+    for(int i = 0; i < 10; i++) {
+        cout << sg[i] << ' ';
+    }
+    /*
+    int n; cin >> n;
+    while(n--) {
+        int a, b;
+        cin >> a >> b;
+        for(int i = a; i <= b; i++) {
+            if(sg[i] != 0) {
+                cout << i << ' ';
+            }
+        }
+        cout << endl;
+    }
+    */
+    return 0;
+}
+
+void sang_nt(vi &a) {
+    for(int i = 2; i < sz(a); i++) {
+        if(a[i] != 0) {
+            for(int j = i*i; j < sz(a); j += i) {
+                a[j] = 0;
+            }
+        }
+    }
+}
