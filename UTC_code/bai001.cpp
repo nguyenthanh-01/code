9cdb62cdb6503bb56cbd524a5cf4bd6f8a39ddda
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-26 23:08:14 +0700

    Update bai001.cpp

--- UTC_code/bai001.cpp
+++ UTC_code/bai001.cpp
@@ -3,42 +3,50 @@
 using namespace std;
 
 #define endl '\n'
-#define sl (int) 1e5+1
+#define sl (int) 1e6+1
 #define sz(a) (int) (a).size()
 
 typedef long long ll;
-typedef vector<int> vi;
+typedef vector<bool> vb;
 
-void sang_nt(vi &);
+void sang_nt(vb &);
+int check(int);
 
 int main() {
-    vi sg(sl, 1); sg[0] = 0;
-    sang_nt(sg);
-    for(int i = 0; i < 10; i++) {
-        cout << sg[i] << ' ';
-    }
-    /*
+    vb sang(sl, 1); sang[0] = 0;
+    sang_nt(sang);
     int n; cin >> n;
     while(n--) {
         int a, b;
         cin >> a >> b;
-        for(int i = a; i <= b; i++) {
-            if(sg[i] != 0) {
+        a = check(a);
+        for(int i = a; i <= b; i += 2) {
+            if(sang[i] != 0) {
                 cout << i << ' ';
             }
         }
         cout << endl;
     }
-    */
     return 0;
 }
 
-void sang_nt(vi &a) {
-    for(int i = 2; i < sz(a); i++) {
+void sang_nt(vb &a) {
+    for(int i = 2; i*i < sz(a); i++) {
         if(a[i] != 0) {
             for(int j = i*i; j < sz(a); j += i) {
                 a[j] = 0;
             }
         }
     }
+}
+
+int check(int a) {
+    if(a < 3) {
+        cout << 2 << ' ';
+        return 3;
+    }
+    if(a%2 == 0) {
+        return a += 1;
+    }
+    return a;
 }
