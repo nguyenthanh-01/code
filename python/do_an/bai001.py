c58afd172b99307e6b76f1940ecd726e247b5a6b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-01-30 22:57:53 +0700

    Update bai001.py

--- python/do_an/bai001.py
+++ python/do_an/bai001.py
@@ -1,55 +1,53 @@
 def main():
-    print(arithmetic_arranger(['3 + 855', '988 + 40'], True))
+    print(arithmetic_arranger(['1 + 1', '2 - 2'], True))
 
-def arithmetic_arranger(problems, val=False):
-    arranged_problems = ''
-    if len(problems) > 5:
-        arranged_problems = "Error: Too many problems."
-        return arranged_problems
+def arithmetic_arranger(problems, re_nhanh = False):
+    so_luong_problems = len(problems)
+    if so_luong_problems > 5:
+        return "Error: Too many problems."
 
-    # list of all operations in str format
-    operations = list(map(lambda x: x.split()[1], problems))
-    for i in set(operations):
-        if i in {'*', '/'}:
-            arranged_problems = "Error: Operator must be '+' or '-'."
-            return arranged_problems
+    cmp = lambda x: x.split()[1]
+    ky_tu = list(map(cmp, problems))
+    for _ in ky_tu:
+        if _ in {'*', '/'}:
+            return "Error: Operator must be '+' or '-'."
 
-    numbers = []  # list of all operands in str format
+    cmp = lambda x: [x[0], x[2]]
+    chu_so = []
     for i in problems:
-        p = i.split()
-        numbers.extend([p[0], p[2]])
+        chu_so += cmp(i.split())
+    
+    for i in chu_so:
+        if not i.isdigit():
+            return "Error: Numbers must only contain digits."
 
-    if not all(map(lambda x: x.isdigit(), numbers)):
-        arranged_problems = "Error: Numbers must only contain digits."
-        return arranged_problems
-
-    if not all(map(lambda x: len(x) < 5, numbers)):
-        arranged_problems = "Error: Numbers cannot be more than four digits."
-        return arranged_problems
-
+        so_luong_chu_so = len(i)
+        if so_luong_chu_so > 4:
+            return "Error: Numbers cannot be more than four digits."
+    
     top_row = ''
     dashes = ''
     values = list(map(lambda x: eval(x), problems))
     solutions = ''
-    for i in range(0, len(numbers), 2):
-        space_width = max(len(numbers[i]), len(numbers[i+1])) + 2
-        top_row += numbers[i].rjust(space_width)
+    for i in range(0, len(chu_so), 2):
+        space_width = max(len(chu_so[i]), len(chu_so[i+1])) + 2
+        top_row += chu_so[i].rjust(space_width)
         dashes += '-' * space_width
         solutions += str(values[i // 2]).rjust(space_width)
-        if i != len(numbers) - 2:
+        if i != len(chu_so) - 2:
             top_row += ' ' * 4
             dashes += ' ' * 4
             solutions += ' ' * 4
 
     bottom_row = ''
-    for i in range(1, len(numbers), 2):
-        space_width = max(len(numbers[i - 1]), len(numbers[i])) + 1
-        bottom_row += operations[i // 2]
-        bottom_row += numbers[i].rjust(space_width)
-        if i != len(numbers) - 1:
+    for i in range(1, len(chu_so), 2):
+        space_width = max(len(chu_so[i - 1]), len(chu_so[i])) + 1
+        bottom_row += ky_tu[i // 2]
+        bottom_row += chu_so[i].rjust(space_width)
+        if i != len(chu_so) - 1:
             bottom_row += ' ' * 4
 
-    if val:
+    if re_nhanh:
         arranged_problems = '\n'.join((top_row, bottom_row, dashes, solutions))
     else:
         arranged_problems = '\n'.join((top_row, bottom_row, dashes))
