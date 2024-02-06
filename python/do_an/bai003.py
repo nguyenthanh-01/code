64324335c058b0ad7834d7d067f61ee29c303d36
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-06 21:49:01 +0700

    Update bai003.py

--- python/do_an/bai003.py
+++ python/do_an/bai003.py
@@ -1,3 +1,21 @@
+def main():
+    food = Category("Food")
+    food.deposit(1000, "initial deposit")
+    food.withdraw(10.15, "groceries")
+    food.withdraw(15.89, "restaurant and more food for dessert")
+    clothing = Category("Clothing")
+    food.transfer(50, clothing)
+    clothing.withdraw(25.55)
+    clothing.withdraw(100)
+    auto = Category("Auto")
+    auto.deposit(1000, "initial deposit")
+    auto.withdraw(15)
+
+    print(food)
+    print()
+
+    print(create_spend_chart([food, clothing, auto]))
+
 class Category:
     def __init__(self, s):
         self.category = s
@@ -58,3 +76,6 @@ def create_spend_chart(categories):
             chart += name[i] + '  '
 
     return chart
+
+if __name__ == "__main__":
+    main()
