a20b9c8f8eb75c39737156e3739e9cc0414fc0ed
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-06 21:52:19 +0700

    Update bai004.py

--- python/do_an/bai004.py
+++ python/do_an/bai004.py
@@ -1,3 +1,22 @@
+def main():
+    rect = Rectangle(10, 5)
+    print(rect.get_area())
+    rect.set_height(3)
+    print(rect.get_perimeter())
+    print(rect)
+    print(rect.get_picture())
+
+    sq = Square(9)
+    print(sq.get_area())
+    sq.set_side(4)
+    print(sq.get_diagonal())
+    print(sq)
+    print(sq.get_picture())
+
+    rect.set_height(8)
+    rect.set_width(16)
+    print(rect.get_amount_inside(sq))
+
 class Rectangle:
     def __init__(self, w, h):
         self.width = w
@@ -43,3 +62,6 @@ class Square(Rectangle):
     def set_side(self, s):
         self.width = s
         self.height = s
+
+if __name__ == "__main__":
+    main()
