add6271a924894c34d3341e55b13a9408c334ca6
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-06 21:59:41 +0700

    Update bai005.py

--- python/do_an/bai005.py
+++ python/do_an/bai005.py
@@ -2,6 +2,14 @@ import copy
 import random
 # Consider using the modules imported above.
 
+def main():
+    random.seed(95)
+    hat = Hat(black=6, red=4, green=3)
+    probability = experiment(hat=hat,
+                  expected_balls={"red":2,"green":1},
+                  num_balls_drawn=5,
+                  num_experiments=2000)
+    print("Probability:", probability)
 
 class Hat:
     def __init__(self, **kwargs):
@@ -21,3 +29,6 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
         m += 1 if balls_req == len(expected_balls) else 0
 
     return m / num_experiments
+
+if __name__ == "__main__":
+    main()
