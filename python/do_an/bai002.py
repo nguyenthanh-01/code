75617cf5d24bde50b2dc32f12cd33b13dd15e3d9
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-07 09:21:41 +0700

    Update bai002.py

--- python/do_an/bai002.py
+++ python/do_an/bai002.py
@@ -2,32 +2,25 @@ def main():
     print(add_time("11:43 PM", "24:20", "tueSday"))
 
 def add_time(start, duration, day=None):
-    # days in a week
     days = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
 
-    # initial time and the duration to be added in hours and minutes
     init_time = list(map(int, start[:-3].split(':')))
     duration = list(map(int, duration.split(':')))
 
-    # adding the minutes to the initial time
     init_time[1] += duration[1]
-    count = 0  # to store the extra number of hours (60 minutes)
+    count = 0
     while init_time[1] > 60:
         count += 1
         init_time[1] -= 60
     
-    # adding the hours to the initial time
     init_time[0] += count + duration[0]
-    count = 0  # to store the number of days
+    count = 0
     while init_time[0] >= 12:
         init_time[0] -= 12
         if 'PM' in start:
-            # increment the count for days if time crosses the PM mark
             count += 1
-            # and replace PM with AM
             start = start.replace('PM', 'AM')
         elif 'AM' in start:
-            # else replace AM with PM
             start = start.replace('AM', 'PM')
 
     init_time[0] = '12' if init_time[0] == 0 else str(init_time[0])
