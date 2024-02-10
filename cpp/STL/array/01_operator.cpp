fd16547eadabaac26d97b9c0ab7a8edc01663069
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-10 18:23:31 +0700

    Update 01_operator.cpp

--- cpp/STL/array/01_operator.cpp
+++ cpp/STL/array/01_operator.cpp
@@ -1,1 +1,18 @@
-.
+#include <bits/stdc++.h>
+using namespace std;
+
+#define n 5
+
+typedef long long ll;
+
+int main() {
+    array<int, n> ar;
+    for (int i = 0; i < n; i++) {
+        cin >> ar[i];
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", ar[i]);
+    }
+    printf("\n");
+    return 0;
+}
