55efba63ba28b80c84dc7c2b11175412d1083df9
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-10 16:09:29 +0700

    Create size.cpp

---
+++ cpp/STL/array/size.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define n 5
+#define sz(a) (int) (a).size()
+
+typedef long long ll;
+
+int main() {
+    array<int, n> ar;
+    printf("%d \n", sz(ar));
+    return 0;
+}
