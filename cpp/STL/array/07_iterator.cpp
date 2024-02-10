c6f08b0ef19ba42fa0b4dea7ee567f0e8171f431
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-11 00:13:42 +0700

    Update 07_iterator.cpp

--- cpp/STL/array/07_iterator.cpp
+++ cpp/STL/array/07_iterator.cpp
@@ -1,1 +1,18 @@
-.
+#include <bits/stdc++.h>
+using namespace std;
+
+#define n 5
+
+typedef long long ll;
+
+int main() {
+    array<int, n> arr;
+    for (auto it = arr.begin(); it != arr.end(); it++) {
+        cin >> *it;
+    }
+    for (auto it = arr.begin(); it != arr.end(); it++) {
+        printf("%d ", *it);
+    }
+    printf("\n");
+    return 0;
+}
