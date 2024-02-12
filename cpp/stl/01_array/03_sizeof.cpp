d1e5c1666da2818d9bd31010c973b7c9765e3270
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-12 17:46:52 +0700

    Create 03_sizeof.cpp

---
+++ cpp/stl/01_array/03_sizeof.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define n 5
+#define sizeof(a) (int) sizeof((a))
+
+int main() {
+    array<int, n> arr;
+    printf("%d \n", sizeof(arr));
+    return 0;
+}
+
+// int, bool
