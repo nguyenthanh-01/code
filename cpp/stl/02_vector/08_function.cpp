9b40978f3cc9f2bc96abe4ee8541e6fdc7850b72
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-12 23:19:45 +0700

    Create 08_function.cpp

---
+++ cpp/stl/02_vector/08_function.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pp pop_back
+#define vector vector<int>
+
+void test(vector &vi);
+
+int main() {
+    int n = 1e9;
+    vector vi(n);
+    test(vi);
+    printf("%d \n", vi.back());
+    return 0;
+}
+
+void test(vector &vi) {
+    vi.back() = 1;
+}
