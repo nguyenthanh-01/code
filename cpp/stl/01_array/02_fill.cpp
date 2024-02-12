7c6d9948291554e1533b810ad1ce42385635f361
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-12 17:42:10 +0700

    Create 02_fill.cpp

---
+++ cpp/stl/01_array/02_fill.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define n 5
+
+int main() {
+    array<int, n> arr;
+    arr.fill(1);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
