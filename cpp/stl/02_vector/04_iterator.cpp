08f9e93522d293e0fcba2382aa4a16181d8fb41f
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-12 20:07:58 +0700

    Update 04_iterator.cpp

--- cpp/stl/02_vector/04_iterator.cpp
+++ cpp/stl/02_vector/04_iterator.cpp
@@ -6,10 +6,9 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-    vector<int> vi;
-    while(n--) {
-        int x; cin >> x;
-        vi.pb(x);
+    vector<int> vi(n);
+    for (int &i: vi) {
+        cin >> i;
     }
     for (auto it = vi.begin(); it != vi.end(); it++) {
         printf("%d ", *it);
