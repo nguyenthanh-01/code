61d0ccd67ae3cd5b1ae6c5779c02e3243b54f70b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-12 19:45:24 +0700

    Create 04_iterator.cpp

---
+++ cpp/stl/02_vector/04_iterator.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pp pop_back
+#define size(a) (int) (a).size()
+
+int main() {
+    int n; cin >> n;
+    vector<int> vi;
+    while(n--) {
+        int x; cin >> x;
+        vi.pb(x);
+    }
+    for (auto it = vi.begin(); it != vi.end(); it++) {
+        printf("%d ", *it);
+    }
+    printf("\n");
+    for (auto it = vi.rbegin(); it != vi.rend(); it++) {
+        printf("%d ", *it);
+    }
+    printf("\n");
+    return 0;
+}
