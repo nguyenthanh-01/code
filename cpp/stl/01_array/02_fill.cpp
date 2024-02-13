ecc5cb5d3da0436c20d6911acd602de26d4e37d8
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-14 00:44:04 +0700

    Update 02_fill.cpp

--- cpp/stl/01_array/02_fill.cpp
+++ cpp/stl/01_array/02_fill.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 #define n 5
+#define array array<bool, n>
 
 int main() {
-    array<bool, n> arr;
+    array arr;
     arr.fill(1);
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
