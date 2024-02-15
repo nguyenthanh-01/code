46a0181e822e7b8e698052deb4f931f764ae5bcf
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-16 05:03:38 +0700

    Create bai001.cpp

---
+++ cpp/bai_tap/bai001.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x; cin >> x;
+    long long res = 1ll * x * x * x + 3ll * x * x + x + 1;
+    cout << res << "\n";
+    return 0;
+}
