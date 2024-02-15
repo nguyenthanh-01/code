b8882e205b4141d24089419ab15bc3fec219f927
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-16 05:12:07 +0700

    Create bai004.cpp

---
+++ cpp/bai_tap/bai004.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int r; cin >> r;
+    double chuvi = 2 * 3.14 * r;
+    double dientich = 3.14 * r * r;
+    printf("%.4lf %.4lf \n", chuvi, dientich);
+    return 0;
+}
