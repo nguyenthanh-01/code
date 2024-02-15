8aac6dd9df0f2a6b28f696258b90009bc092c2ac
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-15 20:18:23 +0700

    Create bai001.py

---
+++ python/cp/arrary/lv1/bai001.py
@@ -0,0 +1,13 @@
+# Python program to implement
+# the above approach
+
+# Driver Code
+V = [ 11, 65, 193, 36, 209, 664, 32 ];
+V.sort()
+V.reverse()
+
+print(f"first = {V[0]}");
+print(f"second = {V[1]}");
+print(f"third = {V[2]}");
+	
+# This code is contributed by Saurabh Jaiswal
