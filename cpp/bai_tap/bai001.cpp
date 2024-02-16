f4358384e56655817a641abe41de275ab16c9382
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-17 00:48:15 +0700

    Delete cpp directory

--- cpp/bai_tap/bai001.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int x; cin >> x;
-    long long res = 1ll * x * x * x + 3ll * x * x + x + 1;
-    cout << res << "\n";
-    return 0;
-}
--- cpp/bai_tap/bai003.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int x; cin >> x;
-    double res = (1.0 * x * 9 / 5) + 32;
-    printf("%.2lf \n", res);
-    return 0;
-}
