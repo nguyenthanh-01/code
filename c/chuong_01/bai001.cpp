d0c6364b2e3ea8cee5c94ca55c479b896efcf190
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-17 00:57:37 +0700

    Create bai001.cpp

---
+++ c/chuong_01/bai001.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    int x; cin >> x;
+    ll res = 1ll * x * x * x + 3ll * x * x + x + 1;
+    cout << res << "\n";
+    return 0;
+}
