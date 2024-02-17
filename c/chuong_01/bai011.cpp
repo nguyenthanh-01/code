35354379e27f2ebb63aaa83ffb8a823968bf20b1
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-18 00:46:00 +0700

    Create bai011.cpp

---
+++ c/chuong_01/bai011.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define x1 a + b > c
+#define y1 a + c > b
+#define z1 b + c > a
+#define x2 a*a + b*b == c*c
+#define y2 a*a + c*c == b*b
+#define z2 b*b + c*c == a*a
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a > 0 && b > 0 && c > 0 && x1 && y1 && z1) {
+        if (a == b && b == c) {
+            cout << "1\n";
+        }
+        else if (a == b || a == c || b == c) {
+            cout << "2\n";
+        }
+        else if (x2 || y2 || z2) {
+            cout << "3\n";
+        }
+        else cout << "4\n";
+    }
+    else cout << "INVALID\n";
+    return 0;
+}
