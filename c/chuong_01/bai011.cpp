cd66b48e268ac2a82dc874b4913cb1d5a5cc2f02
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-18 00:48:47 +0700

    Update bai011.cpp

--- c/chuong_01/bai011.cpp
+++ c/chuong_01/bai011.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define x1 a + b > c
-#define y1 a + c > b
-#define z1 b + c > a
+#define x1 a + b <= c
+#define y1 a + c <= b
+#define z1 b + c <= a
 #define x2 a*a + b*b == c*c
 #define y2 a*a + c*c == b*b
 #define z2 b*b + c*c == a*a
@@ -11,7 +11,10 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    if (a > 0 && b > 0 && c > 0 && x1 && y1 && z1) {
+    if (a <= 0 || b <= 0 || c <= 0 || x1 || y1 || z1) {
+        cout << "INVALID\n";
+    }
+    else {
         if (a == b && b == c) {
             cout << "1\n";
         }
@@ -23,6 +26,5 @@ int main() {
         }
         else cout << "4\n";
     }
-    else cout << "INVALID\n";
     return 0;
 }
