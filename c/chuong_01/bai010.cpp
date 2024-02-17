810a339a8f6c9c7392b6d130980c8ee3c274f0cf
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-17 19:58:05 +0700

    Update bai010.cpp

--- c/chuong_01/bai010.cpp
+++ c/chuong_01/bai010.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define x a + b
-#define y a + c
-#define z b + c
+#define x a + b > c
+#define y a + c > b
+#define z b + c > a
 
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    if (a > 0 && b > 0 && c > 0 && x > c && y > b && z > a) {
+    if (a > 0 && b > 0 && c > 0 && x && y && z) {
         cout << "YES\n";
     }
     else cout << "NO\n";
