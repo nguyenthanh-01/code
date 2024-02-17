9289e58c56aa198f608c5a058bacab09e55551a3
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-17 09:38:00 +0700

    Update bai008.cpp

--- c/chuong_01/bai008.cpp
+++ c/chuong_01/bai008.cpp
@@ -1,15 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << a + b << "\n";
-    cout << a - b << "\n";
-    cout << 1ll * a * b << "\n";
+    int tong = a + b;
+    int hieu = a - b;
+    ll tich = 1ll * a * b;
+    printf("%d \n %d \n %d \n", tong, hieu, tich);
     if (b == 0) {
         cout << "INVALID\n";
     }
-    else printf("%.4lf \n", 1.0 * a / b);
+    else {
+        double thuong = 1.0 * a / b;
+        printf("%.4lf \n", thuong);
+    }
     return 0;
 }
