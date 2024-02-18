c5748493dc21893b1a24b7cf76470c78fca4b586
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-18 21:47:12 +0700

    Create bai012.py

---
+++ python/chuong_01/bai012.py
@@ -0,0 +1,16 @@
+def main():
+    t, n = map(int, input().split())
+    if t < 1 or t > 12 or n <= 0:
+        print("INVALID")
+    else:
+        if t == 2:
+            if n%400 == 0 or (n%4 == 0 and n%100 != 0):
+                print("29")
+            else:
+                print("28")
+        elif t == 4 or t == 6 or t == 9 or t == 11:
+            print("30")
+        else:
+            print("31")
+
+main()
