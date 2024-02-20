7071d25e6e193b6e81622db81a993bc303e41820
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-20 23:52:14 +0700

    Update bai001_sang_nguyen_to.cpp

--- cpp/bai001_sang_nguyen_to.cpp
+++ cpp/bai001_sang_nguyen_to.cpp
@@ -13,7 +13,7 @@ int main() {
 
 void sang_nt() {
     arr.fill(1);
-    arr[0] = arr[1] = 0;
+    arr[0] = arr[1] = false;
     for (int i = 0; i*i < n; i++) {
         if (arr[i] == true) {
             for (int j = i*i; j < n; j += i) {
