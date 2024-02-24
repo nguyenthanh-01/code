7443a25ddf6acc52bd25a442e02d4183f3e6f878
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-02-24 21:33:00 +0700

    Update bai003_sap_xep_noi_bot.cpp

--- cpp/bai003_sap_xep_noi_bot.cpp
+++ cpp/bai003_sap_xep_noi_bot.cpp
@@ -1,40 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define vector vector<int>
+#define n 5
+#define array array<int, n>
 
-void nhap(vector &);
-void xuat(vector &);
-void sx_noi_bot(int, vector &);
+void sx_noi_bot(array &);
+void xuat(array &);
 
 int main() {
-    int n; cin >> n;
-    vector vi(n);
-    nhap(vi);
-    sx_noi_bot(n, vi);
-    xuat(vi);
+    array arr = {1, 2, 3, 4, 5};
+    sx_noi_bot(arr);
+    xuat(arr);
     return 0;
 }
 
-void nhap(vector &vi) {
-    for (int &i: vi) {
-        cin >> i;
-    }
-}
-
-void xuat(vector &vi) {
-    for (int &i: vi) {
-        cout << i << " ";
-    }
-    cout << "\n";
-}
-
-void sx_noi_bot(int n, vector &vi) {
+void sx_noi_bot(array &arr) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (vi[j] < vi[j+1]) {
-                swap(vi[j], vi[j+1]);
+            if (arr[j] < arr[j+1]) {
+                swap(arr[j], arr[j+1]);
             }
         }
     }
+}
+
+void xuat(array &arr) {
+    for (int &i: arr) {
+        cout << i << " ";
+    }
+    cout << "\n";
 }
