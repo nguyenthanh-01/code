562bf1b3897594c9700c59a9b7b24ccbec9d2f52
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-03-01 22:12:48 +0700

    Create bai_e03.cpp

---
+++ cpp/array/bai_e03.cpp
@@ -0,0 +1,21 @@
+/*
+# đề
+input: array
+output: chuyển số 0 về cuối
+*/
+#include <bits/stdc++.h>
+using namespace std;
+                                                                         
+#define n 5
+#define all(a) (a).begin(), (a).end()
+
+inline bool cmp(int &i) {return i != 0;}
+
+int main() {
+    array<int, n> arr = {0, 0, 3, 4, 5};
+    stable_partition(all(arr), cmp);
+    for (int &i: arr) {
+        cout << i << " ";
+    }
+    return 0;
+}
