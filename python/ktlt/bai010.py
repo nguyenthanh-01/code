781219fac8e5c8bf4c25fb874f110ffc9ea36b51
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-05-19 10:41:42 +0700

    Create bai010.py

---
+++ python/ktlt/bai010.py
@@ -0,0 +1,15 @@
+def main():
+    # 48 - 57 0 - 9
+    # 65 - 90 A - Z
+    # 97 - 122 a - z
+
+    print(ord("A"))
+    print(chr(65))
+
+    # islower()
+    # isupper()
+    # isdigit()
+    # isalpha()
+    # isalnum()
+
+main()
