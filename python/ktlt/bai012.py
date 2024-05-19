62217d22b655892b39aab2a19c84715c749b9725
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-05-19 16:22:56 +0700

    Update bai012.py

--- python/ktlt/bai012.py
+++ python/ktlt/bai012.py
@@ -1,7 +1,9 @@
 def main():
     # res = (k^n - 1) // (k - 1) * arr[1]
 
-    n = int(input())
-    print((4**n - 1) // (4 - 1) * 8)
+    arr_1, k, n = map(int, input().split())
+    res = (k**n - 1) // (k - 1) * arr_1
+
+    print(res)
 
 main()
