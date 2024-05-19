87e529451a51bcfccfac9f340e35ad1c8dee0bd8
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-05-19 16:12:30 +0700

    Update bai011.py

--- python/ktlt/bai011.py
+++ python/ktlt/bai011.py
@@ -1,11 +1,12 @@
-
 def main():
     # arr[n] = arr[1] + (n - 1) * k
     # sum = n // 2 * (arr[1] + arr[n])
 
-    n = int(input())
-    arr_n = 1 + (n - 1) * 1
-    sum = n // 2 * (1 + arr_n) + n % 2 * (n + 1) // 2
+    arr_1, k, n = map(int, input().split())
+    arr_n = arr_1 + (n - 1) * k
+    val = arr_1 + arr_n
+    sum = n // 2 * val + n % 2 * val // 2
+
     print(sum)
 
 main()
