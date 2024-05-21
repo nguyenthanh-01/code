8d318acebaf5bb64f05011999f57fed3c716654d
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-05-21 13:27:50 +0700

    Create 000000qd.py

---
+++ utc/000000qd.py
@@ -0,0 +1,22 @@
+def main():
+    n = int(input())
+
+    for i in range(1, n + 1):
+        Str = ""
+
+        if i % 2 == 0:
+            Str += "L"
+        else: Str += f"{i}"
+
+        if i % 4 == 0:
+            Str += "T"
+
+        if i % 8 == 0:
+            Str += "O"
+
+        if i % 16 == 0:
+            Str += "L"
+
+        print(Str)
+
+main()
