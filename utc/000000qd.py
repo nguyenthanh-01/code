a43e25e6f5d72a67a4e37f725d78d8a4a7166a9c
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-05-23 22:09:00 +0700

    Update 000000qd.py

--- utc/000000qd.py
+++ utc/000000qd.py
@@ -4,9 +4,9 @@ def main():
     for i in range(1, n + 1):
         chr = ""
 
-        if i % 2 == 0:
-            chr += "L"
-        else: chr += f"{i}"
+        if i % 2 != 0:
+            chr += f"{i}"
+        else: chr += "L"
 
         if i % 4 == 0:
             chr += "T"
