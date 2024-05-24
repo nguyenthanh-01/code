8e2160729f2f55b7b32245b8bfbf0d531123be17
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-05-24 23:47:18 +0700

    Update 124543456.py

--- utc/124543456.py
+++ utc/124543456.py
@@ -1,11 +1,13 @@
 def main():
     n = int(input())
+    
     sum = 0
     cnt = 0
     max = 0
 
     for i in range(n):
         l, r = map(float, input().split())
+        
         res = l * r
         sum += res
         cnt += 1
@@ -14,6 +16,7 @@ def main():
             max = res
 
     ans = sum / cnt
+    
     print(f"{ans:.3f}")
     print(f"{max:.3f}")
 
