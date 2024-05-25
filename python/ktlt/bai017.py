f924ea42e1650c0049a45bb3aabfc8fcc591cf4b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-05-26 03:24:00 +0700

    Update bai017.py

--- python/ktlt/bai017.py
+++ python/ktlt/bai017.py
@@ -1,11 +1,18 @@
 def main():
     l, r = map(int, input().split())
+
     Arr = [1] * (r - l + 1)
     Sang_nguyen_to(Arr, l, r)
+
     print(Arr)
 
 def Sang_nguyen_to(Arr, l, r):
     Sqrt = int(r**0.5)
+    
+    if l < 2:
+        Arr[0] = 0
+    if l < 1:
+        Arr[1] = 0
 
     for i in range(2, Sqrt + 1):
         Max = max(i * i, (l + i - 1) // i * i)
