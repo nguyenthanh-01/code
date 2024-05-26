af403ef53c4e0b9e09f4024ed0a399d6e7f8a2d2
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-05-26 19:07:06 +0700

    Create bai020.py

---
+++ python/ktlt/bai020.py
@@ -0,0 +1,17 @@
+def main():
+    n = int(input())
+
+    Val = [0]
+    Fibonacci(n - 1, Val)
+
+    print(Val)
+
+def Fibonacci(n, Val):
+    if n == 1 or n == 0:
+        Val[0] += n
+        return
+
+    Fibonacci(n - 1, Val)
+    Fibonacci(n - 2, Val)
+
+main()
