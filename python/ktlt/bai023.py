af1175dff6c84b2fe9c996ae1b4a3462ebde18a3
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-03 08:41:07 +0700

    Update bai023.py

--- python/ktlt/bai023.py
+++ python/ktlt/bai023.py
@@ -1,20 +1,19 @@
 def main():
     a, b = map(int, input().split())
 
-    Old_a = a
-    Check_b = b % 2
-
     if b < 2:
         print(a**b)
         return
 
+    Tmp = 1
+
     while b != 1:
+        if b % 2 != 0:
+            Tmp *= a
+
         a *= a
         b //= 2
 
-    if Check_b != 0:
-        a *= Old_a
-
-    print(a)
+    print(a * Tmp)
 
 main()
