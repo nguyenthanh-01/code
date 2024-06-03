7525143643a21c621adbbf56a68a4b5785368f8b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-04 02:33:20 +0700

    Update bai026.py

--- python/ktlt/bai026.py
+++ python/ktlt/bai026.py
@@ -2,22 +2,22 @@ def main():
     n = int(input())
 
     Min = [10**6]
-    check(n, 0, Min)
+    Check(n, 0, Min)
 
     print(Min)
 
-def check(n, Cnt, Min):
+def Check(n, Cnt, Min):
     if n == 1:
         if Min[0] > Cnt:
             Min[0] = Cnt
         return
 
     if n % 2 == 0:
-        check(n // 2, Cnt + 1, Min)
+        Check(n // 2, Cnt + 1, Min)
     
     if n % 3 == 0:
-        check(n // 3, Cnt + 1, Min)
+        Check(n // 3, Cnt + 1, Min)
 
-    check(n - 1, Cnt + 1, Min)
+    Check(n - 1, Cnt + 1, Min)
 
 main()
