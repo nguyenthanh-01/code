f71c08188ea81d3ca16bcffbbbb7aac634a2dc6c
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-04 11:03:23 +0700

    Update bai028.py

--- python/ktlt/bai028.py
+++ python/ktlt/bai028.py
@@ -5,29 +5,17 @@ def main():
     print(tk_np(arr, x))
 
 def tk_np(arr, x):
-    l = tk_np_(arr, x)
-    r = tk_np_(arr, x, 1)
-    return r - l + 1
-
-def tk_np_(arr, x, tree = 0):
     l = 0
     r = len(arr) - 1
 
-    if tree == 0:
-        pos = -1
-    else:
-        pos = -2
+    pos = -1
 
     while l <= r:
         m = l + (r - l) // 2
 
         if arr[m] == x:
             pos = m
-
-            if tree == 0:
-                r = m - 1
-            else:
-                l = m + 1
+            r = m - 1
 
         elif arr[m] < x:
             l = m + 1
