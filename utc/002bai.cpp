b1b58f623e81e051dd7a27887ef61ff362d70b80
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-07 03:10:36 +0700

    Update 002bai.cpp

--- utc/002bai.cpp
+++ utc/002bai.cpp
@@ -6,44 +6,44 @@ using namespace std;
 void sang_nguyen_to(bool []);
 
 int main() {
-	bool arr[n] = {0};
-	sang_nguyen_to(arr);
-	
-	int m = 0; cin >> m;
-	while(m--) {
-		int a = 0; cin >> a;
-		int b = 0; cin >> b;
-		
-		if(a < 3) {
-			printf("2 ");
-		}
-		
-		a = (a%2 == 0) ? a + 1 : a;
-		
-		for(int i = a; i <= b; i += 2) {
-			if(arr[i] == 1) {
-				printf("%d ", i);
-			}	
-		}
-		
-		printf("\n");
-	}
-	
-	return 0;
+    bool arr[n] = {0};
+    sang_nguyen_to(arr);
+    
+    int m = 0; cin >> m;
+    while(m--) {
+        int a = 0; cin >> a;
+        int b = 0; cin >> b;
+        
+        if(a < 3) {
+            printf("2 ");
+        }
+        
+        a = (a%2 == 0) ? a + 1 : a;
+        
+        for(int i = a; i <= b; i += 2) {
+            if(arr[i] == 1) {
+                printf("%d ", i);
+            }    
+        }
+        
+        printf("\n");
+    }
+    
+    return 0;
 }
 
 void sang_nguyen_to(bool arr[]) {
-	arr[2] = 1;
-	for(int i = 3; i < n; i += 2) {
-		arr[i] = 1;
-	}
-	
-	int tmp = sqrt(n);
-	for(int i = 3; i <= tmp; i += 2) {
-		if(arr[i] == 1) {
- 	        for(int j = i * i; j < n; j += i) {
-				arr[j] = 0;
-		    }
-		}
-	}
+    arr[2] = 1;
+    for(int i = 3; i < n; i += 2) {
+        arr[i] = 1;
+    }
+    
+    int tmp = sqrt(n);
+    for(int i = 3; i <= tmp; i += 2) {
+        if(arr[i] == 1) {
+             for(int j = i * i; j < n; j += i) {
+                arr[j] = 0;
+            }
+        }
+    }
 }
