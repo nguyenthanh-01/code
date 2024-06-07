cd4f1c1020729a31e592560c93b88e0b08774c83
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-07 22:35:30 +0700

    Delete utc directory

--- utc/2019banhdanem.py
+++
@@ -1,9 +0,0 @@
-def main():
-    n = int(input())
-
-    Tmp = 1 if n % 9 != 0 else 0
-    Ans = n // 9 + Tmp
-
-    print(Ans)
-
-main()
--- utc/45.py
+++
@@ -1,8 +0,0 @@
-def main():
-    n, s, t = map(int, input().split())
-
-    Tmp = min(s, t)
-
-    print(n - Tmp + 1)
-
-main()
