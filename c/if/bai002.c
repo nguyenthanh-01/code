ffe7aab001c40239e2fa225b565d733e2f1ae93c
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-23 12:53:58 +0700

    Create bai002.c

---
+++ c/if/bai002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    if(a == 5) {
+        printf("1");
+    }
+    else if(a == 10) {
+        printf("2");
+    }
+    return 0;
+}
