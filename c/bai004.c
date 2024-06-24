1e8bfae7dcc6951be90593d653c9b35d2c3bcd95
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-25 04:12:40 +0700

    Update bai004.c

--- c/bai004.c
+++ c/bai004.c
@@ -4,16 +4,16 @@ int main() {
     int a;
     scanf("%d", &a);
     if(a < 5) {
-        printf("1");
+        printf("1 \n");
     }
     else if(a < 10) {
-        printf("2");
+        printf("2 \n");
     }
     if(a == 5) {
-        printf("3");
+        printf("3 \n");
     }
     else if(a == 10) {
-        printf("4");
+        printf("4 \n");
     }
     return 0;
 }
