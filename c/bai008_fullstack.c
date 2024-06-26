6c4cf6a3683ce84068dd8a9840d25f967363bb04
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-27 00:59:07 +0700

    Update bai008_fullstack.c

--- c/bai008_fullstack.c
+++ c/bai008_fullstack.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 
-void test_1(int, int []);
+const int inf = (int) 1e9;
+
+void test_1(int, int, int []);
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    int a[] = {0};
-    test_1(n, a);
+    int a[] = {inf};
+    int cnt = 0;
+    test_1(n, cnt, a);
 
     printf("%d \n", a[0]);
     return 0;
 }
 
-void test_1(int n, int a[]) {
-    if(n == 0) {
-        return;
-    }
-
+void test_1(int n, int cnt, int a[]) {
     if(n == 1) {
-        a[0]++;
+        if(a[0] > cnt) {
+            a[0] = cnt;
+        }
         return;
     }
 
-    test_1(n - 1, a);
-    test_1(n - 2, a);
+    if(n % 2 == 0) {
+        test_1(n / 2, cnt + 1, a);
+    }
+
+    if(n % 3 == 0) {
+        test_1(n / 3, cnt + 1, a);
+    }
+
+    test_1(n - 1, cnt + 1, a);
 }
