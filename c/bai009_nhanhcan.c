9d9d7901e0ec31cb6fe1c0e2cec23f30cdfaf658
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-26 15:58:24 +0700

    Create bai009_nhanhcan.c

---
+++ c/bai009_nhanhcan.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+const int inf = (int) 1e9;
+
+void test_1(int, int, int []);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[] = {inf};
+    int cnt = 0;
+    test_1(n, cnt, a);
+
+    printf("%d \n", a[0]);
+    return 0;
+}
+
+void test_1(int n, int cnt, int a[]) {
+    if(a[0] == cnt) {
+        return;
+    }
+
+    if(n == 1) {
+        a[0] = cnt;
+        return;
+    }
+
+    if(n % 2 == 0) {
+        test_1(n / 2, cnt + 1, a);
+    }
+
+    if(n % 3 == 0) {
+        test_1(n / 3, cnt + 1, a);
+    }
+
+    test_1(n - 1, cnt + 1, a);
+}
