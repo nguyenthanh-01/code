ad2fda55b0cd6832ee12c0d8a23b0ab54b135f7c
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-26 14:20:31 +0700

    Create bai008_renhanh.c

---
+++ c/bai008_renhanh.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void test_1(int, int []);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[] = {0};
+    test_1(n, a);
+
+    printf("%d \n", a[0]);
+    return 0;
+}
+
+void test_1(int n, int a[]) {
+    if(n <= 1) {
+        a[0] += n;
+        return;
+    }
+
+    test_1(n - 1, a);
+    test_1(n - 2, a);
+}
