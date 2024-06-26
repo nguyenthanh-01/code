0445b210c461d9e7e84ebbf5ac5caf7e84bfe691
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-27 00:48:13 +0700

    Update bai011_dfs_nhanhcan.c

--- c/bai011_dfs_nhanhcan.c
+++ c/bai011_dfs_nhanhcan.c
@@ -3,7 +3,7 @@
 const int inf = (int) 1e9;
 
 void test_1(int, int, int []);
-void test_2(int, int, int []);
+int test_2(int, int, int []);
 
 int main() {
     int n;
@@ -15,7 +15,7 @@ int main() {
     a[0]++;
     test_2(n, cnt, a);
 
-    printf("(%d 0)\n", n);
+    printf("\n");
     return 0;
 }
 
@@ -40,29 +40,34 @@ void test_1(int n, int cnt, int a[]) {
     test_1(n - 1, cnt + 1, a);
 }
 
-void test_2(int n, int cnt, int a[]) {
+int test_2(int n, int cnt, int a[]) {
     if(a[0] == cnt) {
-        return;
-    }
-
-    if(a[1] == 0) {
-        printf("(%d %d)", n, cnt);
-        return;
+        return 0;
     }
 
     if(n == 1) {
-        printf("(%d 6)", n);
-        a[1] = 0;
-        return;
+        printf("%d ", n);
+        return 1;
     }
 
     if(n % 2 == 0) {
-        test_2(n / 2, cnt + 1, a);
+        if(test_2(n / 2, cnt + 1, a)) {
+            printf("%d ", n);
+            return 1;
+        }
     }
 
     if(n % 3 == 0) {
-        test_2(n / 3, cnt + 1, a);
+        if(test_2(n / 3, cnt + 1, a)) {
+            printf("%d ", n);
+            return 1;
+        }
     }
 
-    test_2(n - 1, cnt + 1, a);
+    if(test_2(n - 1, cnt + 1, a)) {
+        printf("%d ", n);
+        return 1;
+    }
+
+    return 0;
 }
