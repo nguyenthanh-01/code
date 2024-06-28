157753ef0bd07c08e2a3bc31a8903b9ee3ac0fe7
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-06-28 17:31:08 +0700

    Create bai012_timkiem_bin.c

---
+++ c/bai012_timkiem_bin.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define size(a) (int) (sizeof(a) / sizeof(a[0]))
+
+void test_1(int [], int [], int, int, int);
+
+int main() {
+    int a[] = {0, 1, 2, 4, 5};
+    int pos[] = {-1};
+    int l = 0;
+    int r = 4;
+    int x;
+    scanf("%d", &x);
+
+    test_1(a, pos, l, r, x);
+
+    printf("vi tri: %d \n", pos[0]);
+    return 0;
+}
+
+void test_1(int a[], int pos[], int l, int r, int x) {
+    if(l > r) {
+        return;
+    }
+
+    int m = l + (r - l) / 2;
+
+    if(a[m] == x) {
+        pos[0] = m;
+        return;
+    }
+
+    if(a[m] < x) {
+        test_1(a, pos, m + 1, r, x);
+    }
+
+    if(a[m] > x) {
+        test_1(a, pos, l, m - 1, x);
+    }
+}
