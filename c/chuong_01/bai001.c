65bf6c96df514012a4494cce85b8a08032237f0b
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-07-02 16:02:40 +0700

    Create bai001.c

---
+++ c/chuong_01/bai001.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+typedef long long ll;
+
+void so_nguyen();
+void so_thuc();
+void ky_tu();
+
+int main() {
+    so_nguyen();
+    so_thuc();
+    ky_tu();
+    return 0;
+}
+
+void so_nguyen() {
+    short a; // 2 byte + %hd
+    int b; // 4 byte + %d
+    ll c; // 8 byte + %lld
+
+    scanf("%hd %d %lld ", &a, &b, &c);
+    printf("%hd %d %lld \n", a, b, c);
+}
+
+void so_thuc() {
+    float a; // 4 byte + %.7f
+    double b; // 8 byte + %.15lf
+
+    scanf("%f %lf ", &a, &b);
+    printf("%.7f %.15lf \n", a, b);
+}
+
+void ky_tu() {
+    char a; // 1 byte + %hd + %c
+
+    scanf("%c ", &a);
+    printf("%hd %c \n", a, a);
+}
