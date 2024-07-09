c89898005cf1d0123bdb048aafd9b300c4fde74a
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-07-10 02:08:42 +0700

    Update bai002_sap_xep_chon.c

--- c/chuong_03/bai002_sap_xep_chon.c
+++ c/chuong_03/bai002_sap_xep_chon.c
@@ -22,14 +22,18 @@ int main() {
     return 0;
 }
 
-void sort(int arr[], int n) {
+void sort(int arr[]) {
+    int n = size(arr);
+    
     for(int i = 0; i < n - 1; i++) {
         int pos = i;
+        
         for(int j = i + 1; j < n; j++) {
             if(arr[pos] < arr[j]) {
                 pos = j;
             }
         }
+        
         int val = arr[i];
         arr[i] = arr[pos];
         arr[pos] = val;
