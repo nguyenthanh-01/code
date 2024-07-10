4bee3ad2268595fa32343c6622544a280ae1c88c
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-07-10 18:25:59 +0700

    Create bai004_cmp_to_key.c

---
+++ c/chuong_03/bai004_cmp_to_key.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define size(a) (int) (sizeof(a) / sizeof(a[0]))
+
+static inline bool cmp(int *a, int *b) {return *a > *b ? true : false;}
+
+void sort(int [], int);
+
+int main() {
+    int arr[] = {3, 2, 1, 4, 5};
+
+    int n = size(arr);
+
+    sort(arr, n);
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+void sort(int arr[], int n) {   
+    for(int i = 0; i < n - 1; i++) {
+        int pos = i;
+        
+        for(int j = i + 1; j < n; j++) {
+            if(cmp(&arr[pos], &arr[j])) {
+                pos = j;
+            }
+        }
+        
+        int val = arr[i];
+        arr[i] = arr[pos];
+        arr[pos] = val;
+    }
+}
