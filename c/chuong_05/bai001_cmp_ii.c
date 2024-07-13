f83f732bad2d7fc0778abe816ab61cbf167ceb02
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-07-13 23:43:31 +0700

    Create bai001_cmp_ii.c

---
+++ c/chuong_05/bai001_cmp_ii.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define size(a) (int) (sizeof(a) / sizeof(a[0]))
+
+typedef struct {int a; int b;} ii;
+
+static inline bool cmp(ii i, ii j) {return i.a > j.a ? true : false;}
+
+void sort(ii [], int);
+
+int main() {
+    ii arr[] = {{3, 4}, {2, 5}, {1, 6}, {4, 3}, {5, 2}};
+
+    int n = size(arr);
+
+    sort(arr, n);
+
+    for(int i = 0; i < n; i++) {
+        printf("(%d, %d) ", arr[i].a, arr[i].b);
+    }
+
+    return 0;
+}
+
+void sort(ii arr[], int n) {   
+    for(int i = 0; i < n - 1; i++) {
+        int pos = i;
+        
+        for(int j = i + 1; j < n; j++) {
+            if(cmp(arr[pos], arr[j])) {
+                pos = j;
+            }
+        }
+        
+        ii val = arr[i];
+        arr[i] = arr[pos];
+        arr[pos] = val;
+    }
+}
