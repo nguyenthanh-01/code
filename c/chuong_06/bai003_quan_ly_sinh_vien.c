c2572054f40906551d0c9ca1e480a6618d15f79f
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-07-18 01:32:48 +0700

    Update bai003_quan_ly_sinh_vien.c

--- c/chuong_06/bai003_quan_ly_sinh_vien.c
+++ c/chuong_06/bai003_quan_ly_sinh_vien.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define n (int) 1e5
 
 typedef struct {char ten[100]; char lop[50]; float diem;} sv;
 
-void display_1();
+void display_0();
 
 int main() {
     sv *arr = (sv*) malloc(n * sizeof(sv));
 
     //int cnt[] = {0};
 
-    display_1();
+    while(true) {
+        display_0();
 
-    int key;
-    scanf("%d", &key);
+        int key;
+        scanf("%d", &key);
+        
+        if(key == 6) {
+            break;
+        }
+    }
 
     free(arr);
     return 0;
 }
 
-void display_1() {
+void display_0() {
     system("cls");
     printf("                                                                          \n");
     printf("                                                                          \n");
