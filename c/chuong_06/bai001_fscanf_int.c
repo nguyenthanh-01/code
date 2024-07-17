e728398ffad2d236d8158ce1610d05ccb0e946b8
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-07-17 14:51:37 +0700

    Create bai001_fscanf_int.c

---
+++ c/chuong_06/bai001_fscanf_int.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define n (int) 1e5
+
+void input(int [], int []);
+
+int main() {
+    int *arr = (int*) calloc(n, sizeof(int));
+
+    int cnt[] = {0};
+
+    input(arr, cnt);
+
+    for(int i = 0; i < cnt[0]; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+    return 0;
+}
+
+void input(int arr[], int cnt[]) {
+    FILE *f = fopen("input.txt", "r");
+
+    while(fscanf(f, "%d ", &arr[cnt[0]]) != EOF) {
+        cnt[0]++;
+    }
+
+    fclose(f);
+}
