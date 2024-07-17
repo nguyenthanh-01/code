f680b9aafa49d1e65e33fed46544d93a64e302be
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-07-17 16:15:32 +0700

    Create bai002_fscanf_struct.c

---
+++ c/chuong_06/bai002_fscanf_struct.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define n (int) 1e5
+#define all(a, i) a[i].ten, a[i].lop, &a[i].diem
+
+typedef struct {char ten[100]; char lop[50]; float diem;} sv;
+
+void input(sv [], int []);
+
+int main() {
+    sv *arr = (sv*) malloc(n * sizeof(sv));
+
+    int cnt[] = {0};
+
+    input(arr, cnt);
+
+    for(int i = 0; i < cnt[0]; i++) {
+        printf("Ten: %s \n", arr[i].ten);
+        printf("Lop: %s \n", arr[i].lop);
+        printf("Diem: %.1f \n", arr[i].diem);
+    }
+
+    free(arr);
+    return 0;
+}
+
+void input(sv arr[], int cnt[]) {
+    FILE *f = fopen("input.txt", "r");
+
+    while(fscanf(f, "%[^\n] %s %f ", all(arr, cnt[0])) != EOF) {
+        cnt[0]++;
+    }
+
+    fclose(f);
+}
