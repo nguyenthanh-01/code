824a74602d38f70f1f43d65044f9af05cfc58dfa
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-07 14:35:07 +0700

    Create bai001.c

---
+++ vxl/8051/bai001.c
@@ -0,0 +1,19 @@
+#include <REGX52.h>
+#include <stdbool.h>
+
+sbit LED_PIN = P0^0;
+
+void Delay_ms(int);
+
+void main() {
+    while(true) {
+        LED_PIN = !LED_PIN;
+        Delay_ms(500);
+    }
+}
+
+void Delay_ms(int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < 123; j++);
+    }
+}
