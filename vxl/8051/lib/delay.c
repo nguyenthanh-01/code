c3c1075d2daf68a5ffadd82d215921ceb8e90767
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-11 22:07:23 +0700

    Update delay.c

--- vxl/8051/lib/delay.c
+++ vxl/8051/lib/delay.c
@@ -1,7 +1,35 @@
 #include "delay.h"
+#include "main.h"
 
-void Delay_ms(int n) {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < 123; j++);
+#if(Hz == 12000000)
+    void Delay_ms(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 123; j++);
+        }
     }
-}
+    
+#elif(Hz == 11059200)
+    void Delay_ms(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 113; j++);
+        }
+    }
+
+#elif(Hz == 8000000)
+    void Delay_ms(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 218; j++);
+        }
+    }
+    
+#elif(Hz == 4000000)
+    void Delay_ms(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 106; j++);
+        }
+    }
+   
+#else
+    #error "ko co ham delay nao phu hop!"
+    
+#endif
