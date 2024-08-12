89e82e55ddef96972a7043c19036aafcb3212215
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-13 06:28:24 +0700

    Update delay.c

--- vxl/8051/lib/delay.c
+++ vxl/8051/lib/delay.c
@@ -3,29 +3,33 @@
 
 #if(Hz == 12000000)
     void Delay_ms(int n) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < 123; j++);
+        int  i, j;
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < 123; j++);
         }
     }
     
 #elif(Hz == 11059200)
     void Delay_ms(int n) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < 113; j++);
+        int i, j;
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < 113; j++);
         }
     }
 
 #elif(Hz == 8000000)
     void Delay_ms(int n) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < 218; j++);
+        int i, j;
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < 218; j++);
         }
     }
     
 #elif(Hz == 4000000)
     void Delay_ms(int n) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < 106; j++);
+        int i, j;
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < 106; j++);
         }
     }
    
