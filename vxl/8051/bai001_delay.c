4569277a8c50605ca6778b145fefaa52c369f183
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-13 06:39:04 +0700

    Update bai001_delay.c

--- vxl/8051/bai001_delay.c
+++ vxl/8051/bai001_delay.c
@@ -1,19 +1,19 @@
 #include <regx52.h>
-#include <stdbool.h>
 
 sbit LED_PIN = P0^0;
 
 void Delay_ms(int);
 
 void main() {
-    while(true) {
+    while(1) {
         LED_PIN = !LED_PIN;
         Delay_ms(500);
     }
 }
 
 void Delay_ms(int n) {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < 123; j++);
+    int i, j;
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < 123; j++);
     }
 }
