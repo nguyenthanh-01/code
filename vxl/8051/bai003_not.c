e69b78c9109008793a74da529b5805f805553df1
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-13 06:40:39 +0700

    Update bai003_not.c

--- vxl/8051/bai003_not.c
+++ vxl/8051/bai003_not.c
@@ -1,19 +1,19 @@
 #include <regx52.h>
-#include <stdbool.h>
 
 void Delay_ms(int);
 
 void main() {
     P0 = 0;
     
-    while(true) {
+    while(1) {
         P0 = ~P0;
         Delay_ms(500);
     }
 }
 
 void Delay_ms(int n) {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < 123; j++);
+    int i, j;
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < 123; j++);
     }
 }
