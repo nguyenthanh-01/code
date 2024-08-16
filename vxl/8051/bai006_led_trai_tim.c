bedd4dab1e2a2850286fc0387e9eefadf4be4d1e
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-16 14:14:51 +0700

    Create bai006_led_trai_tim.c

---
+++ vxl/8051/bai006_led_trai_tim.c
@@ -0,0 +1,24 @@
+#include "../lib/at89c52.h"
+
+void main() {
+    while(1) {
+        int i;
+
+        P0 = P1 = P2 = P3 = 0;
+
+        P0 = 0x01;
+
+        for(i = 0; i < 4; i++) {
+            P0 |= (1 << i);
+        }
+
+        for(i = 0; i < 4; i++) {
+            P0 <<= 1;
+        }
+
+        for(i = 0; i < 4; i++) {
+            P0 <<= 1;
+            P1 |= (1 << i)
+        }
+    }
+}
