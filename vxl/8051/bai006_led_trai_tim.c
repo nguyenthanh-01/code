831149aea5f1e98a12c6c789bb19239dd70a427d
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-16 17:20:47 +0700

    Update bai006_led_trai_tim.c

--- vxl/8051/bai006_led_trai_tim.c
+++ vxl/8051/bai006_led_trai_tim.c
@@ -1,5 +1,7 @@
 #include "../lib/at89c52.h"
 
+void Nhat();
+
 void main() {
     int i;
     int n;
@@ -7,64 +9,68 @@ void main() {
     P0 = P1 = P2 = P3 = 0;
 
     while(1) {
+        Nhat();
+    }
+}
+
+void Nhat() {
+    for(i = 0; i < 4; i++) {
+        P0 |= (1 << i);
+        Delay_ms(100);
+    }
+        
+    n = 2;
+
+    while(n--) {
         for(i = 0; i < 4; i++) {
-            P0 |= (1 << i);
+            P0 <<= 1;
+            Delay_ms(100);
+        }
+
+        for(i = 0; i < 4; i++) {
+            P0 <<= 1;
+            P1 |= (1 << i);
             Delay_ms(100);
         }
         
-        n = 2;
+        for(i = 0; i < 4; i++) {
+            P1 <<= 1;
+            Delay_ms(100);
+        }
 
-        while(n--) {
-            for(i = 0; i < 4; i++) {
-                P0 <<= 1;
-                Delay_ms(100);
-            }
-
-            for(i = 0; i < 4; i++) {
-                P0 <<= 1;
-                P1 |= (1 << i);
-                Delay_ms(100);
-            }
+        for(i = 0; i < 4; i++) {
+            P1 <<= 1;
+            P2 |= (1 << i);
+            Delay_ms(100);
+        }
         
-            for(i = 0; i < 4; i++) {
-                P1 <<= 1;
-                Delay_ms(100);
-            }
+        for(i = 0; i < 4; i++) {
+            P2 <<= 1;
+            Delay_ms(100);
+        }
 
-            for(i = 0; i < 4; i++) {
-                P1 <<= 1;
-                P2 |= (1 << i);
-                Delay_ms(100);
-            }
+        for(i = 0; i < 4; i++) {
+            P2 <<= 1;
+            P3 |= (1 << i);
+            Delay_ms(100);
+        }
         
-            for(i = 0; i < 4; i++) {
-                P2 <<= 1;
-                Delay_ms(100);
-            }
+        for(i = 0; i < 4; i++) {
+            P3 <<= 1;
+            Delay_ms(100);
+        }
 
+        if(n != 0) {
             for(i = 0; i < 4; i++) {
-                P2 <<= 1;
-                P3 |= (1 << i);
+                P3 <<= 1;
+                P0 |= (1 << i);
                 Delay_ms(100);
             }
-        
+        }
+        else {
             for(i = 0; i < 4; i++) {
                 P3 <<= 1;
                 Delay_ms(100);
-            }
-
-            if(n != 0) {
-                for(i = 0; i < 4; i++) {
-                    P3 <<= 1;
-                    P0 |= (1 << i);
-                    Delay_ms(100);
-                }
-            }
-            else {
-                for(i = 0; i < 4; i++) {
-                    P3 <<= 1;
-                    Delay_ms(100);
-                }
             }
         }
     }
