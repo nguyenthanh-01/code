288cc6df37b13a721e7d060b25b3637edfcf18df
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-16 16:44:50 +0700

    Update bai006_led_trai_tim.c

--- vxl/8051/bai006_led_trai_tim.c
+++ vxl/8051/bai006_led_trai_tim.c
@@ -1,22 +1,63 @@
 #include "../lib/at89c52.h"
 
 void main() {
-    while(1) {
-        int i;
+    int i;
+    int n;
 
+    while(1) {
         P0 = P1 = P2 = P3 = 0;
 
         for(i = 0; i < 4; i++) {
             P0 |= (1 << i);
+            Delay_ms(100);
         }
+        
+        n = 2;
 
-        for(i = 0; i < 4; i++) {
-            P0 <<= 1;
-        }
+        while(n--) {
+            for(i = 0; i < 4; i++) {
+                P0 <<= 1;
+                Delay_ms(100);
+            }
 
-        for(i = 0; i < 4; i++) {
-            P0 <<= 1;
-            P1 |= (1 << i)
+            for(i = 0; i < 4; i++) {
+                P0 <<= 1;
+                P1 |= (1 << i);
+                Delay_ms(100);
+            }
+        
+            for(i = 0; i < 4; i++) {
+                P1 <<= 1;
+                Delay_ms(100);
+            }
+
+            for(i = 0; i < 4; i++) {
+                P1 <<= 1;
+                P2 |= (1 << i);
+                Delay_ms(100);
+            }
+        
+            for(i = 0; i < 4; i++) {
+                P2 <<= 1;
+                Delay_ms(100);
+            }
+
+            for(i = 0; i < 4; i++) {
+                P2 <<= 1;
+                P3 |= (1 << i);
+                Delay_ms(100);
+            }
+        
+            for(i = 0; i < 4; i++) {
+                P3 <<= 1;
+                Delay_ms(100);
+            }
+
+            for(i = 0; i < 4; i++) {
+                P3 <<= 1;
+                P0 |= (1 << i);
+                Delay_ms(100);
+            }
         }
     }
 }
