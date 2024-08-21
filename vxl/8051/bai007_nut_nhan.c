76b624171ab6b42b9ed95bf4e63f9c07f015a9df
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-21 19:39:50 +0700

    Update bai007_nut_nhan.c

--- vxl/8051/bai007_nut_nhan.c
+++ vxl/8051/bai007_nut_nhan.c
@@ -1,14 +1,18 @@
 #include "../lib/at89c52.h"
 
+sbit On = P1^0;
+sbit Off = P1^1;
+sbit Led = P2^0;
+
 void main() {
-    P2_0 = 0;
+    Led = 0;
 
     while(1) {
-        if(P1_0 == 0) {
-            P2_0 = 1;
+        if(On == 0) {
+            Led = 1;
         }
-        esle {
-            P2_0 = 0;
+        if(Off == 0) {
+            Led = 0;
         }
     }
 }
