1836df82da806b48d51468ac102a94b28c7a19cc
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-22 12:34:05 +0700

    Create bai008_nut_nhan_03.c

---
+++ vxl/8051/bai008_nut_nhan_03.c
@@ -0,0 +1,36 @@
+#include "../lib/at89c52.h"
+
+#define Led P2
+
+sbit On = P1^0;
+sbit Off = P1^1;
+
+void Delay_Off(int);
+
+void main() {
+    Led = 0;
+
+    while(1) {
+        On = 1;
+        if(On == 0) {
+            Led = 1;
+        }
+        
+        Delay_Off(1000);
+    }
+}
+
+void Delay_Off(int n) {
+    int i;
+    n /= 10;
+
+    for(i = 0; i < n; i++) {
+        Delay_ms(10);
+
+        Off = 1;
+        if(Off == 0) {
+            Led = 0;
+            return;
+        }
+    }
+}
