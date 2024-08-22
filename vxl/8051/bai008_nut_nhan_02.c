f1f7f8bebfa72386feca5cb0dcdd8106b74ca010
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-22 17:58:10 +0700

    Update bai008_nut_nhan_02.c

--- vxl/8051/bai008_nut_nhan_02.c
+++ vxl/8051/bai008_nut_nhan_02.c
@@ -1,14 +1,14 @@
 #include "../lib/at89c52.h"
 
 void main() {
-    Led = 0;
+    Led_bit = 0;
 
     while(1) {
         if(On_Off == 0) {
             Delay_ms(20);
             
             if(On_Off == 0) {
-                Led = !Led;
+                Led_bit = !Led_bit;
             
                 while(On_Off == 0);
             }
