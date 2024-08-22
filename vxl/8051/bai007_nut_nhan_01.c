f526b3641f20ead473341466927a4db90c8761ed
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-22 17:57:00 +0700

    Update bai007_nut_nhan_01.c

--- vxl/8051/bai007_nut_nhan_01.c
+++ vxl/8051/bai007_nut_nhan_01.c
@@ -1,17 +1,17 @@
 #include "../lib/at89c52.h"
 
 void main() {
-    Led = 0;
+    Led_bit = 0;
 
     while(1) {
         On = 1;
         if(On == 0) {
-            Led = 1;
+            Led_bit = 1;
         }
         
         Off = 1;
         if(Off == 0) {
-            Led = 0;
+            Led_bit = 0;
         }
     }
 }
