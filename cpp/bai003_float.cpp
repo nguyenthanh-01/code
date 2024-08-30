fd95302fe802539535ad1c2433a979cc2a4cf176
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-30 19:06:38 +0700

    Update bai003_float.cpp

--- cpp/bai003_float.cpp
+++ cpp/bai003_float.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define fx(a) fixed << setprecision(a)
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     float x = 3.145982;
 
-    cout << fixed << setprecision(2) << x;
+    cout << fx(2) << x;
     return 0;
 }
