eac0e9fc8930c03ffcacee73541ee5ce95b0b6d0
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-30 18:53:43 +0700

    Create bai002_int.cpp

---
+++ cpp/bai002_int.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << INT_MIN << endl;
+    cout << INT_MAX << endl;
+    return 0;
+}
