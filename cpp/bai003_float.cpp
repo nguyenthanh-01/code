2dae2048226faf8ba236f4ca088943d913ae367d
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-08-30 19:04:20 +0700

    Create bai003_float.cpp

---
+++ cpp/bai003_float.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    float x = 3.145982;
+
+    cout << fixed << setprecision(2) << x;
+    return 0;
+}
