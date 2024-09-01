22b7485cf4ba4bb5c2703e2c27b986e8f4c25afd
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-09-01 13:36:20 +0700

    Create bai002_mang_danh_dau.cpp

---
+++ cpp/chuong01_map/bai002_mang_danh_dau.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define fi first
+#define se second
+
+typedef map<int, int> mp;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    int arr[n];
+    mp sub;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sub[arr[i]]++;
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(sub[arr[i]] != 0) {
+            cout << arr[i] << " " << sub[arr[i]] << "\n";
+            sub[arr[i]] = 0;
+        }
+    }
+
+    return 0;
+}
