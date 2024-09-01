956bd2d5e8809ccad44f358be39cdac5571e984e
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-09-01 13:18:34 +0700

    Update and rename bai001.cpp to bai001_mang_danh_dau.cpp

---
+++ cpp/chuong01_map/bai001_mang_danh_dau.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define fi first
+#define se second
+
+typedef map<int, int> mp;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    int arr[n];
+    mp sub;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sub[arr[i]]++;
+    }
+
+    for(auto it : sub) {
+        cout << "(" << it.fi << ", " << it.se << ")\n";
+    }
+
+    return 0;
+}
