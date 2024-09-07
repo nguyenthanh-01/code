be0e784b65cb8396a49a83adf0a0322eadb40d71
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-09-07 10:07:25 +0700

    Update bai003_tim_kiem_nhi_phan.c

--- c/chuong_03/bai003_tim_kiem_nhi_phan.c
+++ c/chuong_03/bai003_tim_kiem_nhi_phan.c
@@ -2,7 +2,8 @@
 
 #define size(a) (int) (sizeof(a) / sizeof(a[0]))
 
-int search(int [], int, int, int);
+int lower_bound(int [], int, int, int);
+int upper_bound(int [], int, int, int);
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
@@ -12,23 +13,27 @@ int main() {
     int x;
     scanf("%d ", &x);
 
-    int vi_tri = search(arr, 0, n - 1, x);
+    int lb = lower_bound(arr, 0, n - 1, x);
 
-    if(vi_tri == -1) {
+    if(lb < 0) {
         printf("khong tim thay \n");
     }
     else {
-        printf("vi tri: %d \n", vi_tri);
+        printf("vi tri: %d \n", lb);
     }
+
     return 0;
 }
 
-int search(int arr[], int l, int r, int x) {
+int lower_bound(int arr[], int l, int r, int x) {
+    int pos = -1;
+
     while(l <= r) {
         int m = l + (r - l) / 2;
 
         if(arr[m] == x) {
-            return m;
+            pos = m;
+            r = m - 1;
         }
         else if(arr[m] > x) {
             r = m - 1;
@@ -37,5 +42,27 @@ int search(int arr[], int l, int r, int x) {
             l = m + 1;
         }
     }
-    return -1;
+
+    return pos;
+}
+
+int upper_bound(int arr[], int l, int r, int x) {
+    int pos = -1;
+
+    while(l <= r) {
+        int m = l + (r - l) / 2;
+
+        if(arr[m] == x) {
+            pos = m;
+            l = m + 1;
+        }
+        else if(arr[m] > x) {
+            r = m - 1;
+        }
+        else {
+            l = m + 1;
+        }
+    }
+
+    return pos;
 }
