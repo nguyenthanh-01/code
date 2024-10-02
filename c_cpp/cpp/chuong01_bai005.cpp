d0e2dc3cd1061ec180695c9cb49ad4e2a07579fb
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-03 00:20:46 +0700

    Create chuong01_bai005.cpp

---
+++ c_cpp/cpp/chuong01_bai005.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    int x = x1 - x2;
+    int y = y1 - y2;
+
+    float khoang_cach = sqrt(x * x + y * y);
+    printf("Khoang cach = %.2f", khoang_cach);
+
+    return 0;
+}
