edc5b320e64cd2d70dd33a618a11e8d261f3dd24
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-02 23:26:21 +0700

    Create chuong01_bai003.c

---
+++ c_cpp/c/chuong01_bai003.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define ll long long
+
+int main() {
+    int C;
+    scanf("%d", &C);
+
+    float F = ((float) C * 9 / 5) + 32;
+    printf("F = %.2f", F);
+
+    return 0;
+}
