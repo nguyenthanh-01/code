5fd8b8319dea13a93ffa5f0033f70d2156b81ec8
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-02 17:00:22 +0700

    Create bai001.c

---
+++ c/chuong01/bai_lam/bai001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define ll long long
+
+int main() {
+    int x;
+    scanf("%d", &x);
+
+    ll A = (ll) x * x * x + (ll) x * x * 3 + x + 1;
+    printf("A = %lld", A);
+
+    return 0;
+}
