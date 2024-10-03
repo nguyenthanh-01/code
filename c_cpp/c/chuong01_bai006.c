09706063e5d08eab3bb46fd85e19cc6e62b8f8fb
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-03 12:42:45 +0700

    Create chuong01_bai006.c

---
+++ c_cpp/c/chuong01_bai006.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define ll long long
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    if(N%2 == 0) {
+        printf("chan");
+    }
+    else {
+        printf("le");
+    }
+
+    return 0;
+}
