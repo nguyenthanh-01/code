e1da7fb9e44a604c77b40d92f1fef4d895876272
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-03 14:51:04 +0700

    Create chuong01_bai009.c

---
+++ c_cpp/c/chuong01_bai009.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define ll long long
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n % 400 == 0 || (n%4 == 0 && n % 100 != 0)) {
+        printf("yes");
+    }
+    else {
+        printf("no");
+    }
+
+    return 0;
+}
