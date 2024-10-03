213d405c36b6e5662556e3b17790d89c89af5bb9
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-04 02:11:44 +0700

    Create chuong02_bai003.c

---
+++ c_cpp/c/chuong02_bai003.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define ll long long
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    ll sum = 0;
+
+    for(int i = 0; i <= n; i += 3) {
+        sum += i;
+    }
+
+    printf("Tong = %lld", sum);
+
+    return 0;
+}
