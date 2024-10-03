7abeb92dbba1f2111f785bfc3480510699dfb6af
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-04 02:13:34 +0700

    Create chuong02_bai003.cpp

---
+++ c_cpp/cpp/chuong02_bai003.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    int n; cin >> n;
+    ll sum = 0;
+
+    for(int i = 0; i <= n; i += 3) {
+        sum += i;
+    }
+
+    cout << "Tong = " << sum << "\n";
+
+    return 0;
+}
