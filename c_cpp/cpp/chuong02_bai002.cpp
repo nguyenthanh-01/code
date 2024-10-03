9b686a56504b01f8ccce8ef34db010248d47f980
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-04 02:00:21 +0700

    Create chuong02_bai002.cpp

---
+++ c_cpp/cpp/chuong02_bai002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    int n; cin >> n;
+
+    ll sum = (ll) n * (n + 1) * (2 * n + 1) / 6;
+    cout << "S = " << sum << "\n";
+
+    return 0;
+}
