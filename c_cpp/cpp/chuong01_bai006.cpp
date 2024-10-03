e40f82f7639b9a82d51a87ff221214d56306f130
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-03 12:47:19 +0700

    Create chuong01_bai006.cpp

---
+++ c_cpp/cpp/chuong01_bai006.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N; cin >> N;
+
+    if(N%2 == 0) {
+        cout << "chan";
+    }
+    else {
+        cout << "le";
+    }
+
+    return 0;
+}
