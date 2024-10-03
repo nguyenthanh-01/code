814a0c349730c1d7f1a6bbfa0c2166cfe866e931
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-03 15:00:31 +0700

    Create chuong01_bai009.cpp

---
+++ c_cpp/cpp/chuong01_bai009.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    int n; cin >> n;
+
+    if(n % 400 == 0 || (n%4 == 0 && n % 100 != 0)) {
+        cout << "yes";
+    }
+    else {
+        cout << "no";
+    }
+
+    return 0;
+}
