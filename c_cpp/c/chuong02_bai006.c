6305350c490cf2f154bff77f6371d9df9abc63bc
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-05 00:25:43 +0700

    Update chuong02_bai006.c

--- c_cpp/c/chuong02_bai006.c
+++ c_cpp/c/chuong02_bai006.c
@@ -8,17 +8,17 @@ int main() {
     scanf("%lld", &n);
 
     ll sum = 0;
-    int _n = sqrt(n);
-    ll n2 = (ll) _n * _n;
+    int m = sqrt(n);
+    ll m2 = (ll) m * m;
 
-    for(int i = 1; i <= _n; i++) {
+    for(int i = 1; i <= m; i++) {
         if(n%i == 0) {
             sum += (i + n / i);
         }
     }
 
-    if(n2 == n) {
-        sum -= _n;
+    if(m2 == n) {
+        sum -= m;
     }
 
     printf("Tong = %lld", sum);
