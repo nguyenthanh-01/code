1c0dd9bde34fd034fa6929f52f8c60bde208fdce
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-05 01:57:32 +0700

    Create chuong02_bai009.cpp

---
+++ c_cpp/cpp/chuong02_bai009.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ll n; cin >> n;
+    ll res = 1;
+    int m = sqrt(n);
+
+    for(int i = 1; i < m; i++) {
+        if(n%i == 0) {
+            res *= ((ll) i * n / i);
+        }
+    }
+
+    if(n%m == 0) {
+        res *= m;
+    }
+
+    cout << "Tich = " << res << "\n";
+
+    return 0;
+}
