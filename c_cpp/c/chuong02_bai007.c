4a28509a9098c83ae302300e77a6671da2a47dde
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-05 01:31:47 +0700

    Update chuong02_bai007.c

--- c_cpp/c/chuong02_bai007.c
+++ c_cpp/c/chuong02_bai007.c
@@ -7,16 +7,16 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    int _n = sqrt(n) + 1;
+    int m = sqrt(n);
 
-    for(int i = 1; i < _n; i++) {
+    for(int i = 1; i < m; i++) {
         if(n%i == 0) {
-            printf("%d ", i);
-            
-            if(i != n / i) {
-                printf("%d ", n / i);
-            }
+            printf("%d %d ", i, n / i);
         }
+    }
+
+    if(n%m == 0) {
+        printf("%d ", m);
     }
 
     return 0;
