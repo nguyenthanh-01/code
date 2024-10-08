57fc2da72d114b7ca1414396258f34eccdcd37de
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-09 01:49:11 +0700

    Create chuong03_bai004.c

---
+++ c_cpp/c/chuong03_bai004.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+#define ll long long
+
+int main() {
+    ll a, b;
+    scanf("%lld %lld", &a, &b);
+
+    int c = ceil(sqrt(a));
+    int d = sqrt(b) + 1;
+
+    for(int i = c; i < d; i++) {
+        printf("%lld ", 1ll * i * i);
+    }
+    return 0;
+}
