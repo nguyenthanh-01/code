2d7521a63abd1768256fd1e5ce9e110591f02b3a
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-09 10:17:23 +0700

    Update chuong01_bai004.c

--- c_cpp/c/chuong01_bai004.c
+++ c_cpp/c/chuong01_bai004.c
@@ -7,8 +7,8 @@ int main() {
     int R;
     scanf("%d", &R);
 
-    float chu_vi = 1.0 * 2 * Pi * R;
-    float dien_tich = 1.0 * Pi * R * R;
+    float chu_vi = 2.0 * Pi * R;
+    float dien_tich = Pi * R * R;
 
     printf("Chu vi = %.4f \n", chu_vi);
     printf("Dien tich = %.4f \n", dien_tich);
