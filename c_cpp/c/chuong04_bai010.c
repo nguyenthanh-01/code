6ac599b1423f24b34bd30132f5b206e67f2d2166
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-12 00:44:03 +0700

    Create chuong04_bai010.c

---
+++ c_cpp/c/chuong04_bai010.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define ll long long
+
+void nhanh_can(int, int, int *);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int cnt = 0;
+    int can = 1000000000;
+
+    nhanh_can(n, cnt, &can);
+
+    printf("%d", can);
+
+    return 0;
+}
+
+void nhanh_can(int n, int cnt, int *can) {
+    if(cnt == *can) {
+        return;
+    }
+
+    if(n == 1) {
+        *can = cnt;
+        return;
+    }
+
+    if(n%2 == 0) {
+        nhanh_can(n / 2, cnt + 1, can);
+    }
+
+    if(n%3 == 0) {
+        nhanh_can(n / 3, cnt + 1, can);
+    }
+
+    nhanh_can(n - 1, cnt + 1, can);
+}
