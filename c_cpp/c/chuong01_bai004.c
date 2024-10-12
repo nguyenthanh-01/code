14e4b3331af6d4bd241eb0c14bf63f34c04d01c1
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-13 03:27:26 +0700

    Update chuong01_bai004.c

--- c_cpp/c/chuong01_bai004.c
+++ c_cpp/c/chuong01_bai004.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 #define ll long long
-#define Pi 3.14
+#define PI 3.14
 
 int main() {
     int r;
     scanf("%d", &r);
 
-    float chu_vi = 2.0 * Pi * r;
-    float dien_tich = Pi * r * r;
+    float chu_vi = 2.0 * PI * r;
+    float dien_tich = PI * r * r;
 
     printf("%.4f \n", chu_vi);
     printf("%.4f \n", dien_tich);
