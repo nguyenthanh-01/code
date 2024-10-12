1b0b14d68b228f6e1f224d96f991f7d06926b2ad
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-13 02:03:48 +0700

    Update chuong02_bai002.c

--- c_cpp/c/chuong02_bai002.c
+++ c_cpp/c/chuong02_bai002.c
@@ -6,8 +6,8 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    ll sum = 1ll * n * (n + 1) * (2 * n + 1) / 6;
-    printf("S = %lld", sum);
+    ll sum = (ll) n * (n + 1) * (2 * n + 1) / 6;
+    printf("%lld", sum);
 
     return 0;
 }
