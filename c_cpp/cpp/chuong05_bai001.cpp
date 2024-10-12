07bbd143fe991c1660456f43a902f465e3a09358
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-12 15:58:48 +0700

    Create chuong05_bai001.cpp

---
+++ c_cpp/cpp/chuong05_bai001.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int k; cin >> k;
+    int cnt = 0;
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == k) {
+                cnt++;
+            }
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
