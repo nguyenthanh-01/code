127b820e7e26b40fc4bd009995d2ad7025d0cc13
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-13 03:38:45 +0700

    Update chuong01_bai001.cpp

--- c_cpp/cpp/chuong01_bai001.cpp
+++ c_cpp/cpp/chuong01_bai001.cpp
@@ -6,8 +6,8 @@ using ll = long long;
 int main() {
     int x; cin >> x;
     
-    ll A = (ll) x * x * x + (ll) x * x * 3 + x + 1;
-    cout << "A = " << A;
+    ll a = (ll) x * x * x + (ll) x * x * 3 + x + 1;
+    cout << a;
 
     return 0;
 }
