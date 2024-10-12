34b8a24bc330c1475256315aa991f5d8e2f0ef9f
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-13 01:19:45 +0700

    Update chuong01_bai002.c

--- c_cpp/c/chuong01_bai002.c
+++ c_cpp/c/chuong01_bai002.c
@@ -6,8 +6,8 @@ int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
 
-    ll S = 1ll * a * (b + c) + 1ll * b * (a + c);
-    printf("S = %lld", S);
+    ll s = 1ll * a * (b + c) + 1ll * b * (a + c);
+    printf("%lld", s);
 
     return 0;
 }
