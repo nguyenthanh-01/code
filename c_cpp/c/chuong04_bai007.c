c333544f9f21663a661988646ce1323164c31cf2
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-13 02:24:18 +0700

    Update chuong04_bai007.c

--- c_cpp/c/chuong04_bai007.c
+++ c_cpp/c/chuong04_bai007.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 
 #define ll long long
-#define mod 1000000007
 
-ll bin_luy_thua(ll, ll);
+const int Mod = (int) 1e9 + 7;
+
+ll luy_thua_bin(ll, ll);
 
 int main() {
     ll a, b;
     scanf("%lld %lld", &a, &b);
 
-    ll ans = bin_luy_thua(a, b);
+    ll ans = luy_thua_bin(a, b);
 
     printf("%lld", ans);
 
     return 0;
 }
 
-ll bin_luy_thua(ll a, ll b) {
+ll luy_thua_bin(ll a, ll b) {
     ll res = 1;
 
     while(b != 0) {
         if(b%2 == 0) {
-            a = (a * a) % mod;
+            a = (a * a) % Mod;
             b /= 2;
         }
         else {
-            res = (res * a) % mod;
+            res = (res * a) % Mod;
             b -= 1;
         }
     }
