23a795ebae34b9a3665fd248875d2f22c21a7fb3
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-15 17:43:27 +0700

    Create chuong05_bai007.cpp

---
+++ c_cpp/cpp/chuong05_bai007.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+#define sz(a) (int) (a).size()
+#define all(a) (a).begin(), (a).end()
+#define pb push_back
+#define pp pop_back
+
+typedef vector<int> vi;
+
+int main() {
+    int n; cin >> n;
+
+    vi arr;
+
+    while(n--) {
+        int y; cin >> y;
+        arr.pb(y);
+    }
+
+    vi arr_sum;
+    arr_sum.pb(arr[0]);
+
+    for(int i = 1; i < sz(arr); i++) {
+        arr_sum.pb(arr_sum[i - 1] + arr[i]);
+    }
+
+    for(int i : arr_sum) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
