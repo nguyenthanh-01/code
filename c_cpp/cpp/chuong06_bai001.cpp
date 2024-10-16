f1ca086666161143fcadace458554da1a1b845b1
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-16 19:18:30 +0700

    Create chuong06_bai001.cpp

---
+++ c_cpp/cpp/chuong06_bai001.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+
+#define n (int) 1e6
+
+bool arr[n];
+
+void sang_nt();
+
+int main() {
+    sang_nt();
+
+    int x;
+
+    while(cin >> x) {
+        if(arr[x] == true) {
+            printf("yes ");
+        }
+        else {
+            printf("no ");
+        }
+    }
+
+    return 0;
+}
+
+void sang_nt() {
+    arr[2] = true;
+
+    for(int i = 3; i < n; i += 2) {
+        arr[i] = true;
+    }
+
+    int _n = sqrt(n) + 1;
+
+    for(int i = 3; i < _n; i += 2) {
+        if(arr[i] == true) {
+            for(int j = i * i; j < n; j += i) {
+                arr[j] = false;
+            }
+        }
+    }
+}
