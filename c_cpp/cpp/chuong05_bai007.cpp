4dfe47db326409e8fc5e0947832805067070c734
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-17 14:42:55 +0700

    Update chuong05_bai007.cpp

--- c_cpp/cpp/chuong05_bai007.cpp
+++ c_cpp/cpp/chuong05_bai007.cpp
@@ -14,11 +14,10 @@ typedef vector<int> vi;
 int main() {
     int n; cin >> n;
 
-    vi arr;
+    vi arr(n);
 
-    while(n--) {
-        int y; cin >> y;
-        arr.pb(y);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
     vi arr_sum;
