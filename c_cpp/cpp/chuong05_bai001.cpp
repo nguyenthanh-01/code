b0e4f7737e9196f7b3b957bd384cee06f175f357
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-18 08:38:16 +0700

    Update chuong05_bai001.cpp

--- c_cpp/cpp/chuong05_bai001.cpp
+++ c_cpp/cpp/chuong05_bai001.cpp
@@ -10,8 +10,8 @@ int main() {
     int n; cin >> n;
     int arr[n];
 
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
+    for(int &i : arr) {
+        cin >> i;
     }
 
     int k; cin >> k;
