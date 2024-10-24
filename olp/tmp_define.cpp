5fde03bf1377880368fa2ca158dc97c117d90201
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-24 08:57:33 +0700

    Create tmp_define.cpp

---
+++ olp/tmp_define.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define MASK(i) (1LL << (i))
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define SET_ON(x, i) ((x) | MASK(i))
+#define SET_OFF(x, i) ((x) & ~MASK(i))
+#define FOR(i, a, b) for(int i = (a), _b = (b); i < _b; i++)
+#define REP(i, n) for(int i = 0, _n = (n); i < _n; i++)
+
+const int MOD = (int) 1e9 + 7;
+const double PI = acos(-1);
+
+inline bool cmp(int &a, int &b) {return a > b;}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
