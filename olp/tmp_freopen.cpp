f0f50b2cac0c71caa2f454ad138648d22177e35e
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-24 09:39:05 +0700

    Create tmp_freopen.cpp

---
+++ olp/tmp_freopen.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main() {
+  	freopen("input.txt", "r", stdin);
+  	freopen("output.txt", "w", stdout);
+
+  	int a;
+  	int b;
+  	int c;
+	
+    scanf("%d %d %d", &a, &b, &c);
+
+  	printf("%d \n", a + b + c);
+
+    return 0;
+}
