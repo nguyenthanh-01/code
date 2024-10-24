408e033b0b74f8c3cc6c5e1679cb1ecb4e81e456
Nguyễn Thành <nguyenthanh.5s4d@gmail.com>
2024-10-24 09:39:58 +0700

    Update tmp_freopen.cpp

--- olp/tmp_freopen.cpp
+++ olp/tmp_freopen.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main() {
-  	freopen("input.txt", "r", stdin);
-  	freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
-  	int a;
-  	int b;
-  	int c;
+    int a;
+    int b;
+    int c;
 	
     scanf("%d %d %d", &a, &b, &c);
 
-  	printf("%d \n", a + b + c);
+    printf("%d \n", a + b + c);
 
     return 0;
 }
