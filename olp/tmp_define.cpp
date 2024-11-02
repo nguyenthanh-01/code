dd2251d6516ec866f0cc1592612457bd8cd9a230
Nguyễn Bằng Thành <nguyenthanh.5s4d@gmail.com>
2024-11-02 23:39:04 +0700

    Update tmp_define.cpp

--- olp/tmp_define.cpp
+++ olp/tmp_define.cpp
@@ -6,8 +6,10 @@
 using namespace std;
 
 using ll = long long;
+using ii = struct {int a; int b;};
 
 using vi = vector<int>;
+using vii = vector<ii>;
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
 #define sz(x) (int) (x).size()
@@ -31,6 +33,7 @@ const int MOD = (int) 1e9 + 7;
 const double PI = acos(-1);
 
 inline bool cmp(int &a, int &b) {return a > b;}
+inline bool cmp_ii(ii &i, ii &j) {return i.a > j.a;}
 
 void setIO();
 
